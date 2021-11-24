7f425c59f8785226299376f07eee91a0c8389313
weixian.cxy <weixian.cxy@alibaba-inc.com>
2021-11-24 16:28:01 +0800

    support config partition type

--- plugins/flusher/kafka/flusher_kafka.go
+++ plugins/flusher/kafka/flusher_kafka.go
@@ -27,14 +27,15 @@ import (
 )
 
 type FlusherKafka struct {
-	context      ilogtail.Context
-	Brokers      []string
-	SASLUsername string
-	SASLPassword string
-	Topic        string
-	ClientID     string
-	isTerminal   chan bool
-	producer     sarama.AsyncProducer
+	context         ilogtail.Context
+	Brokers         []string
+	SASLUsername    string
+	SASLPassword    string
+	Topic           string
+	PartitionerType string
+	ClientID        string
+	isTerminal      chan bool
+	producer        sarama.AsyncProducer
 }
 
 func (k *FlusherKafka) Init(context ilogtail.Context) error {
@@ -55,7 +56,14 @@ func (k *FlusherKafka) Init(context ilogtail.Context) error {
 	config.ClientID = k.ClientID
 	config.Producer.Return.Successes = true
 	// config.Producer.RequiredAcks = sarama.WaitForAll
-	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	partitioner := sarama.NewRandomPartitioner
+	switch k.PartitionerType {
+	case "roundrobin":
+		partitioner = sarama.NewRoundRobinPartitioner
+	case "hash":
+		partitioner = sarama.NewHashPartitioner
+	}
+	config.Producer.Partitioner = partitioner
 	config.Producer.Timeout = 5 * time.Second
 	producer, err := sarama.NewAsyncProducer(k.Brokers, config)
 	if err != nil {
