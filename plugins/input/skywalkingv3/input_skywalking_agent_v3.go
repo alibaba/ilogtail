4d19fbfd089951792afb069fa9a38a63c41a29ac
千乘 <gavin.zx@alibaba-inc.com>
2022-05-07 11:46:12 +0800

    Refine code

--- plugins/input/skywalkingv3/input_skywalking_agent_v3.go
+++ plugins/input/skywalkingv3/input_skywalking_agent_v3.go
@@ -21,6 +21,7 @@ import (
 
 	"github.com/alibaba/ilogtail"
 
+	configuration "github.com/alibaba/ilogtail/plugins/input/skywalkingv3/skywalking/network/agent/configuration/v3"
 	agent "github.com/alibaba/ilogtail/plugins/input/skywalkingv3/skywalking/network/language/agent/v3"
 	profile "github.com/alibaba/ilogtail/plugins/input/skywalkingv3/skywalking/network/language/profile/v3"
 	logging "github.com/alibaba/ilogtail/plugins/input/skywalkingv3/skywalking/network/logging/v3"
@@ -67,6 +68,7 @@ func (r *Input) Start(collector ilogtail.Collector) error {
 	agent.RegisterTraceSegmentReportServiceServer(r.grpcServer, &TracingHandler{r.ctx, collector, resourcePropertiesCache, InitComponentMapping(r.ComponentMapping)})
 	management.RegisterManagementServiceServer(r.grpcServer, &ManagementHandler{r.ctx, collector, resourcePropertiesCache})
 	profile.RegisterProfileTaskServer(r.grpcServer, &ProfileHandler{})
+	configuration.RegisterConfigurationDiscoveryServiceServer(r.grpcServer, &ConfigurationDiscoveryHandler{})
 	logging.RegisterLogReportServiceServer(r.grpcServer, &loggingHandler{r.ctx, collector})
 	if r.Address == "" {
 		r.Address = "0.0.0.0:11800" // skywalking collector default port
