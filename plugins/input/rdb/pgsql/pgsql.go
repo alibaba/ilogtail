1c71634f60dcabc12508c84d182320f8b066bf52
panawala <wei782566232@126.com>
2022-08-09 19:29:08 +0800

    update imports

--- plugins/input/rdb/pgsql/pgsql.go
+++ plugins/input/rdb/pgsql/pgsql.go
@@ -21,8 +21,6 @@ import (
 	"strconv"
 	"strings"
 
-	_ "github.com/jackc/pgx/v4/stdlib" //nolint:blank-imports
-
 	"github.com/alibaba/ilogtail"
 	"github.com/alibaba/ilogtail/pkg/util"
 	"github.com/alibaba/ilogtail/plugins/input/rdb"
