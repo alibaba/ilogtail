05f0f9d081c2540c57a4dda19ae5ceebe02153c7
linrunqi08 <linrunqi08@163.com>
2022-08-11 14:32:10 +0800

    rm unuse code

--- core/parser/LogParser.cpp
+++ core/parser/LogParser.cpp
@@ -728,7 +728,6 @@ bool LogParser::ApsaraEasyReadLogLineParser(const char* buffer,
     sprintf(s_micro, "%lld", logTime_in_micro);
 #endif
     AddLog(logPtr, "microtime", string(s_micro), logGroupSize);
-    AddLog(logPtr, "rawtime", timeStr, logGroupSize);
     return true;
 }
 
