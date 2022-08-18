693816a35186b00a1af3e3cdcad21f2409619e41
Takuka0311 <1914426213@qq.com>
2022-08-18 18:08:10 +0800

    machine api

--- ilogtail_controller/ConfigServer/controller/user/machine.go
+++ ilogtail_controller/ConfigServer/controller/user/machine.go
@@ -1,6 +1,17 @@
 package user
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/alibaba/ilogtail/ilogtail_controller/ConfigServer/common"
+	ilogtailmanager "github.com/alibaba/ilogtail/ilogtail_controller/ConfigServer/service/ilogtail_manager"
+	"github.com/gin-gonic/gin"
+)
 
 func ListAllMachines(c *gin.Context) {
+	machineList, err := ilogtailmanager.GetAllMachine()
+
+	if err != nil {
+		c.JSON(500, common.Error(common.InternalServerError, err.Error()))
+	} else {
+		c.JSON(200, common.Accept("Get machine list success", gin.H{"machineList": machineList}))
+	}
 }
