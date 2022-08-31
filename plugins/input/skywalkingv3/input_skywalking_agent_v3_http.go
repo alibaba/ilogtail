6381f76520dfea28913537e889ce4b495427f0f7
千乘 <gavin.zx@alibaba-inc.com>
2022-08-31 10:06:55 +0800

    Fix lint issue

--- plugins/input/skywalkingv3/input_skywalking_agent_v3_http.go
+++ plugins/input/skywalkingv3/input_skywalking_agent_v3_http.go
@@ -17,19 +17,21 @@ package skywalkingv3
 import (
 	"context"
 	"encoding/json"
-	common "github.com/alibaba/ilogtail/plugins/input/skywalkingv3/skywalking/network/common/v3"
-	v3 "github.com/alibaba/ilogtail/plugins/input/skywalkingv3/skywalking/network/language/agent/v3"
-	loggingV3 "github.com/alibaba/ilogtail/plugins/input/skywalkingv3/skywalking/network/logging/v3"
-	management "github.com/alibaba/ilogtail/plugins/input/skywalkingv3/skywalking/network/management/v3"
-	"google.golang.org/protobuf/encoding/protojson"
 	"net/http"
 	"time"
 
 	"github.com/alibaba/ilogtail"
 	"github.com/alibaba/ilogtail/pkg/logger"
+
+	"google.golang.org/protobuf/encoding/protojson"
+
+	common "github.com/alibaba/ilogtail/plugins/input/skywalkingv3/skywalking/network/common/v3"
+	v3 "github.com/alibaba/ilogtail/plugins/input/skywalkingv3/skywalking/network/language/agent/v3"
+	loggingV3 "github.com/alibaba/ilogtail/plugins/input/skywalkingv3/skywalking/network/logging/v3"
+	management "github.com/alibaba/ilogtail/plugins/input/skywalkingv3/skywalking/network/management/v3"
 )
 
-type SkywalkingHttpServerInput struct {
+type SkywalkingHTTPServerInput struct {
 	Address          string
 	server           *http.Server
 	context          ilogtail.Context
@@ -39,16 +41,16 @@ type SkywalkingHttpServerInput struct {
 type parameterParser func(request *http.Request) (interface{}, error)
 type urlHandler func(parameter interface{}) (interface{}, error)
 
-func (s *SkywalkingHttpServerInput) Init(context ilogtail.Context) (int, error) {
+func (s *SkywalkingHTTPServerInput) Init(context ilogtail.Context) (int, error) {
 	s.context = context
 	return 0, nil
 }
 
-func (s *SkywalkingHttpServerInput) Description() string {
+func (s *SkywalkingHTTPServerInput) Description() string {
 	return "This is a skywalking v3 http server input"
 }
 
-func (s *SkywalkingHttpServerInput) Start(collector ilogtail.Collector) error {
+func (s *SkywalkingHTTPServerInput) Start(collector ilogtail.Collector) error {
 	logger.Info(s.context.GetRuntimeContext(), "Start skywalking v3 http server")
 	mux := http.NewServeMux()
 
@@ -76,15 +78,15 @@ func (s *SkywalkingHttpServerInput) Start(collector ilogtail.Collector) error {
 		collector: collector,
 	}
 
-	mux.HandleFunc("/v3/segment", registerTraceSegmentHandler(traceHandler))
-	mux.HandleFunc("/v3/segments", registerTraceSegmentsHandler(traceHandler))
-	mux.HandleFunc("/v3/management/reportProperties", registerReportPropertiesHandler(managementHandler))
-	mux.HandleFunc("/v3/management/keepAlive", registerKeepAliveHandler(managementHandler))
-	mux.HandleFunc("/v3/logs", registerLogHandler(loggingHandler))
-	mux.HandleFunc("/v3/events", registerEventHandler())
-	mux.HandleFunc("/browser/errorLog", registerErrorLogHandler(loggingHandler))
-	mux.HandleFunc("/browser/errorLogs", registerErrorLogsHandler(loggingHandler))
-	mux.HandleFunc("/browser/perfData", registerPerfData(&perDataHandler{s.context, collector}))
+	mux.HandleFunc("/v3/segment", s.registerTraceSegmentHandler(traceHandler))
+	mux.HandleFunc("/v3/segments", s.registerTraceSegmentsHandler(traceHandler))
+	mux.HandleFunc("/v3/management/reportProperties", s.registerReportPropertiesHandler(managementHandler))
+	mux.HandleFunc("/v3/management/keepAlive", s.registerKeepAliveHandler(managementHandler))
+	mux.HandleFunc("/v3/logs", s.registerLogHandler(loggingHandler))
+	mux.HandleFunc("/v3/events", s.registerEventHandler())
+	mux.HandleFunc("/browser/errorLog", s.registerErrorLogHandler(loggingHandler))
+	mux.HandleFunc("/browser/errorLogs", s.registerErrorLogsHandler(loggingHandler))
+	mux.HandleFunc("/browser/perfData", s.registerPerfData(&perDataHandler{s.context, collector}))
 	if s.Address == "" {
 		s.Address = "0.0.0.0:12800"
 	}
@@ -97,8 +99,8 @@ func (s *SkywalkingHttpServerInput) Start(collector ilogtail.Collector) error {
 	return s.server.ListenAndServe()
 }
 
-func registerPerfData(handler *perDataHandler) func(http.ResponseWriter, *http.Request) {
-	return registerHandler(func(request *http.Request) (interface{}, error) {
+func (s *SkywalkingHTTPServerInput) registerPerfData(handler *perDataHandler) func(http.ResponseWriter, *http.Request) {
+	return s.registerHandler(func(request *http.Request) (interface{}, error) {
 		var browserPerfData *v3.BrowserPerfData
 		return browserPerfData, json.NewDecoder(request.Body).Decode(browserPerfData)
 	}, func(parameter interface{}) (interface{}, error) {
@@ -106,20 +108,28 @@ func registerPerfData(handler *perDataHandler) func(http.ResponseWriter, *http.R
 	})
 }
 
-func registerErrorLogsHandler(handler errorLogsHandler) func(http.ResponseWriter, *http.Request) {
-	return registerHandler(func(request *http.Request) (interface{}, error) {
+func (s *SkywalkingHTTPServerInput) registerErrorLogsHandler(handler errorLogsHandler) func(http.ResponseWriter, *http.Request) {
+	return s.registerHandler(func(request *http.Request) (interface{}, error) {
 		loggingData := make([]*v3.BrowserErrorLog, 0)
 		dec := json.NewDecoder(request.Body)
-		for dec.Token(); dec.More(); {
-			var result map[string]interface{}
-			dec.Decode(&result)
+		_, e := dec.Token()
+		if e != nil {
+			return nil, e
+		}
 
-			d, _ := json.Marshal(result)
-			log := &v3.BrowserErrorLog{}
-			if e := protojson.Unmarshal(d, log); e != nil {
+		for dec.More() {
+			var result map[string]interface{}
+			if e := dec.Decode(&result); e != nil {
+				logger.Error(s.context.GetRuntimeContext(), "Failed to decode error log", e)
 				continue
+			} else {
+				d, _ := json.Marshal(result)
+				log := &v3.BrowserErrorLog{}
+				if e := protojson.Unmarshal(d, log); e != nil {
+					continue
+				}
+				loggingData = append(loggingData, log)
 			}
-			loggingData = append(loggingData, log)
 		}
 		return loggingData, nil
 	}, func(parameter interface{}) (interface{}, error) {
@@ -127,8 +137,8 @@ func registerErrorLogsHandler(handler errorLogsHandler) func(http.ResponseWriter
 	})
 }
 
-func registerErrorLogHandler(handler errorLogHandler) func(http.ResponseWriter, *http.Request) {
-	return registerHandler(func(request *http.Request) (interface{}, error) {
+func (s *SkywalkingHTTPServerInput) registerErrorLogHandler(handler errorLogHandler) func(http.ResponseWriter, *http.Request) {
+	return s.registerHandler(func(request *http.Request) (interface{}, error) {
 		loggingData := &v3.BrowserErrorLog{}
 		return loggingData, json.NewDecoder(request.Body).Decode(loggingData)
 	}, func(parameter interface{}) (interface{}, error) {
@@ -136,16 +146,16 @@ func registerErrorLogHandler(handler errorLogHandler) func(http.ResponseWriter, 
 	})
 }
 
-func registerEventHandler() func(http.ResponseWriter, *http.Request) {
-	return registerHandler(func(request *http.Request) (interface{}, error) {
+func (s *SkywalkingHTTPServerInput) registerEventHandler() func(http.ResponseWriter, *http.Request) {
+	return s.registerHandler(func(request *http.Request) (interface{}, error) {
 		return nil, nil
 	}, func(parameter interface{}) (interface{}, error) {
 		return &common.Commands{}, nil
 	})
 }
 
-func registerLogHandler(handler loggingDataHandler) func(http.ResponseWriter, *http.Request) {
-	return registerHandler(func(request *http.Request) (interface{}, error) {
+func (s *SkywalkingHTTPServerInput) registerLogHandler(handler loggingDataHandler) func(http.ResponseWriter, *http.Request) {
+	return s.registerHandler(func(request *http.Request) (interface{}, error) {
 		loggingData := &loggingV3.LogData{}
 		return loggingData, json.NewDecoder(request.Body).Decode(loggingData)
 	}, func(parameter interface{}) (interface{}, error) {
@@ -153,8 +163,8 @@ func registerLogHandler(handler loggingDataHandler) func(http.ResponseWriter, *h
 	})
 }
 
-func registerKeepAliveHandler(handler keepAliveHandler) func(http.ResponseWriter, *http.Request) {
-	return registerHandler(func(request *http.Request) (interface{}, error) {
+func (s *SkywalkingHTTPServerInput) registerKeepAliveHandler(handler keepAliveHandler) func(http.ResponseWriter, *http.Request) {
+	return s.registerHandler(func(request *http.Request) (interface{}, error) {
 		instancePingPkg := &management.InstancePingPkg{}
 		return instancePingPkg, json.NewDecoder(request.Body).Decode(instancePingPkg)
 	}, func(parameter interface{}) (interface{}, error) {
@@ -162,8 +172,8 @@ func registerKeepAliveHandler(handler keepAliveHandler) func(http.ResponseWriter
 	})
 }
 
-func registerReportPropertiesHandler(handler reportInstancePropertiesHandler) func(http.ResponseWriter, *http.Request) {
-	return registerHandler(func(request *http.Request) (interface{}, error) {
+func (s *SkywalkingHTTPServerInput) registerReportPropertiesHandler(handler reportInstancePropertiesHandler) func(http.ResponseWriter, *http.Request) {
+	return s.registerHandler(func(request *http.Request) (interface{}, error) {
 		instanceProperties := &management.InstanceProperties{}
 		return instanceProperties, json.NewDecoder(request.Body).Decode(instanceProperties)
 	}, func(parameter interface{}) (interface{}, error) {
@@ -171,8 +181,8 @@ func registerReportPropertiesHandler(handler reportInstancePropertiesHandler) fu
 	})
 }
 
-func registerTraceSegmentHandler(handler traceSegmentHandler) func(writer http.ResponseWriter, request *http.Request) {
-	return registerHandler(func(request *http.Request) (interface{}, error) {
+func (s *SkywalkingHTTPServerInput) registerTraceSegmentHandler(handler traceSegmentHandler) func(writer http.ResponseWriter, request *http.Request) {
+	return s.registerHandler(func(request *http.Request) (interface{}, error) {
 		traceSegment := &v3.SegmentObject{}
 		return traceSegment, json.NewDecoder(request.Body).Decode(traceSegment)
 	}, func(parameter interface{}) (interface{}, error) {
@@ -180,20 +190,28 @@ func registerTraceSegmentHandler(handler traceSegmentHandler) func(writer http.R
 	})
 }
 
-func registerTraceSegmentsHandler(handler traceSegmentsHandler) func(writer http.ResponseWriter, request *http.Request) {
-	return registerHandler(func(request *http.Request) (interface{}, error) {
+func (s *SkywalkingHTTPServerInput) registerTraceSegmentsHandler(handler traceSegmentsHandler) func(writer http.ResponseWriter, request *http.Request) {
+	return s.registerHandler(func(request *http.Request) (interface{}, error) {
 		segments := make([]*v3.SegmentObject, 0)
 		dec := json.NewDecoder(request.Body)
-		for dec.Token(); dec.More(); {
-			var result map[string]interface{}
-			dec.Decode(&result)
+		_, e := dec.Token()
+		if e != nil {
+			return nil, e
+		}
 
-			d, _ := json.Marshal(result)
-			segment := &v3.SegmentObject{}
-			if e := protojson.Unmarshal(d, segment); e != nil {
+		for dec.More() {
+			var result map[string]interface{}
+			if e := dec.Decode(&result); e != nil {
+				logger.Error(s.context.GetRuntimeContext(), "Failed to decode error log", e)
 				continue
+			} else {
+				d, _ := json.Marshal(result)
+				segment := &v3.SegmentObject{}
+				if e := protojson.Unmarshal(d, segment); e != nil {
+					continue
+				}
+				segments = append(segments, segment)
 			}
-			segments = append(segments, segment)
 		}
 		return segments, nil
 	}, func(data interface{}) (interface{}, error) {
@@ -201,35 +219,41 @@ func registerTraceSegmentsHandler(handler traceSegmentsHandler) func(writer http
 	})
 }
 
-func registerHandler(parser parameterParser, handler urlHandler) func(writer http.ResponseWriter, request *http.Request) {
+func (s *SkywalkingHTTPServerInput) registerHandler(parser parameterParser, handler urlHandler) func(writer http.ResponseWriter, request *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
 		if parameter, e := parser(request); e != nil {
 			writer.WriteHeader(http.StatusBadRequest)
-			writer.Write([]byte("Failed to parse request"))
+			s.writeResponse(writer, []byte("Failed to parse request"))
 		} else {
 			if e, result := handler(parameter); e != nil {
 				writer.WriteHeader(http.StatusInternalServerError)
-				writer.Write([]byte("Failed to handle request"))
+				s.writeResponse(writer, []byte("Failed to handle request"))
 			} else {
 				if r, err := json.Marshal(result); err != nil {
 					writer.WriteHeader(http.StatusBadRequest)
-					writer.Write([]byte("Failed to marshal result"))
+					s.writeResponse(writer, []byte("Failed to marshal result"))
 				} else {
-					writer.Write(r)
+					s.writeResponse(writer, r)
 				}
 			}
 		}
 	}
 }
 
-func (s *SkywalkingHttpServerInput) Stop() error {
+func (s *SkywalkingHTTPServerInput) writeResponse(writer http.ResponseWriter, data []byte) {
+	if _, e := writer.Write(data); e != nil {
+		logger.Error(s.context.GetRuntimeContext(), "Failed to write response", e)
+	}
+}
+
+func (s *SkywalkingHTTPServerInput) Stop() error {
 	logger.Info(s.context.GetRuntimeContext(), "close skywalking v3 http server")
 	return s.server.Shutdown(context.Background())
 }
 
 func init() {
 	ilogtail.ServiceInputs["service_skywalking_agent_v3_http"] = func() ilogtail.ServiceInput {
-		return &SkywalkingHttpServerInput{
+		return &SkywalkingHTTPServerInput{
 			Address: "0.0.0.0:12800",
 		}
 	}
