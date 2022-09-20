00b83f7c36cd5d235d1fd239528db83bdfa3625e
Evan <31562192+EvanLjp@users.noreply.github.com>
2022-09-20 17:38:05 +0800

    polish jmxfetch (#407)

---
+++ helper/k8s_test.go
@@ -0,0 +1,46 @@
+package helper
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExtractPodWorkload(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "pod",
+			args: args{
+				name: "kube-state-metrics-86679c9454-5rmck",
+			},
+			want: "kube-state-metrics",
+		},
+
+		{
+			name: "set",
+			args: args{
+				name: "kube-state-metrics-5rmck",
+			},
+			want: "kube-state-metrics",
+		},
+		{
+			name: "statefulset",
+			args: args{
+				name: "kube-state-metrics-0",
+			},
+			want: "kube-state-metrics",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, ExtractPodWorkload(tt.args.name), "ExtractPodWorkload(%v)", tt.args.name)
+		})
+	}
+}
