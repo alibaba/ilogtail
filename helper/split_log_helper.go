50362360ab9f91a830b52c0962e0e21bba8d1de8
liuhaoyang <liuhaoyang.qz@bytedance.com>
2022-10-19 11:28:41 +0800

    fix lint:don't use ALL_CAPS in Go names; use CamelCase

--- helper/split_log_helper.go
+++ helper/split_log_helper.go
@@ -6,11 +6,11 @@ import (
 	"github.com/alibaba/ilogtail/pkg/protocol"
 )
 
-var FILE_OFFSET_KEY = "__tag__:__file_offset__"
+var FileOffsetKey = "__tag__:__file_offset__"
 
 func ReviseFileOffset(log *protocol.Log, offset int64) {
 	for _, cont := range log.Contents {
-		if cont.Key == FILE_OFFSET_KEY {
+		if cont.Key == FileOffsetKey {
 			if beginOffset, err := strconv.ParseInt(cont.Value, 10, 64); err == nil {
 				cont.Value = strconv.FormatInt(beginOffset+offset, 10)
 			}
