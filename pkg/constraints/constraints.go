c33e18c8a09ecbe3c9ae57ee154b58762d1c3078
liuhaoyang <liuhaoyang.qz@bytedance.com>
2022-12-03 12:01:42 +0800

    reset constraints.IntUintFloat

--- pkg/constraints/constraints.go
+++ pkg/constraints/constraints.go
@@ -15,5 +15,5 @@
 package constraints
 
 type IntUintFloat interface {
-	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64
+	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64
 }
