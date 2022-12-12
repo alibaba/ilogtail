cd240e95328b170ef946959c77e9604b769ac3bf
guogangping <guogangping@bytedance.com>
2022-12-12 03:09:10 +0000

    fix: gofmt

--- helper/decoder/influxdb/decoder_test.go
+++ helper/decoder/influxdb/decoder_test.go
@@ -19,9 +19,9 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/alibaba/ilogtail/pkg/protocol"
-	
 	"github.com/stretchr/testify/assert"
+
+	"github.com/alibaba/ilogtail/pkg/protocol"
 )
 
 var textFormat = `
