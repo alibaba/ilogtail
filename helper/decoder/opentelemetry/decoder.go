81fed19d1fa24da9fb8cd1396164e281e268c475
Takuka0311 <1914426213@qq.com>
2022-12-20 20:00:39 +0800

    cannot reflect to ilogtail

--- helper/decoder/opentelemetry/decoder.go
+++ helper/decoder/opentelemetry/decoder.go
@@ -19,8 +19,11 @@ import (
 	"errors"
 	"net/http"
 	"strconv"
+	"time"
 
 	"go.opentelemetry.io/collector/pdata/plog/plogotlp"
+	"go.opentelemetry.io/collector/pdata/pmetric"
+	"go.opentelemetry.io/collector/pdata/pmetric/pmetricotlp"
 
 	"github.com/alibaba/ilogtail/helper/decoder/common"
 	"github.com/alibaba/ilogtail/pkg/protocol"
@@ -35,6 +38,9 @@ const (
 type Decoder struct {
 	Format string
 }
+
+type jsonKVData map[string]interface{}
+type jsonArrayData []interface{}
 
 // Decode impl
 func (d *Decoder) Decode(data []byte, req *http.Request) (logs []*protocol.Log, err error) {
@@ -56,7 +62,7 @@ func (d *Decoder) Decode(data []byte, req *http.Request) (logs []*protocol.Log, 
 			err = errors.New("Invalid ContentType: " + req.Header.Get("Content-Type"))
 		}
 	case common.ProtocolOTLPMetricV1:
-		otlpMetricReq := plogotlp.NewRequest()
+		otlpMetricReq := pmetricotlp.NewRequest()
 		switch req.Header.Get("Content-Type") {
 		case pbContentType:
 			if err = otlpMetricReq.UnmarshalProto(data); err != nil {
@@ -137,56 +143,122 @@ func (d *Decoder) ConvertOtlpLogV1(otlpLogReq plogotlp.Request) (logs []*protoco
 	return logs, nil
 }
 
-func (d *Decoder) ConvertOtlpMetricV1(otlpLogReq plogotlp.Request) (logs []*protocol.Log, err error) {
-	resLogs := otlpLogReq.Logs().ResourceLogs()
-	for i := 0; i < resLogs.Len(); i++ {
-		resourceLog := resLogs.At(i)
-		sLogs := resourceLog.ScopeLogs()
-		for j := 0; j < sLogs.Len(); j++ {
-			scopeLog := sLogs.At(j)
-			lRecords := scopeLog.LogRecords()
-			for k := 0; k < lRecords.Len(); k++ {
-				logRecord := lRecords.At(k)
+func (d *Decoder) ConvertOtlpMetricV1(otlpMetricReq pmetricotlp.Request) (logs []*protocol.Log, err error) {
+	resMetrics := otlpMetricReq.Metrics().ResourceMetrics()
+	for i := 0; i < resMetrics.Len(); i++ {
+		resourceMetric := resMetrics.At(i)
+		sMetrics := resourceMetric.ScopeMetrics()
+		for j := 0; j < sMetrics.Len(); j++ {
+			scopeMetric := sMetrics.At(j)
+			metrics := scopeMetric.Metrics()
+			for k := 0; k < metrics.Len(); k++ {
+				metric := metrics.At(k)
 
 				protoContents := []*protocol.Log_Content{
 					{
-						Key:   "time_unix_nano",
-						Value: strconv.FormatInt(logRecord.Timestamp().AsTime().UnixNano(), 10),
+						Key:   "name",
+						Value: metric.Name(),
 					},
 					{
-						Key:   "severity_number",
-						Value: strconv.FormatInt(int64(logRecord.SeverityNumber()), 10),
+						Key:   "description",
+						Value: metric.Description(),
 					},
 					{
-						Key:   "severity_text",
-						Value: logRecord.SeverityText(),
+						Key:   "unit",
+						Value: metric.Unit(),
 					},
 					{
-						Key:   "content",
-						Value: logRecord.Body().AsString(),
+						Key:   "data_type",
+						Value: metric.DataType().String(),
 					},
 				}
 
-				if logRecord.Attributes().Len() != 0 {
-					if d, err := json.Marshal(logRecord.Attributes().AsRaw()); err == nil {
+				switch metric.DataType() {
+				case pmetric.MetricDataTypeGauge:
+					gauge := metric.Gauge()
+					dataPoints := jsonArrayData{}
+					for l := 0; l < gauge.DataPoints().Len(); l++ {
+						dataPoints = append(dataPoints, gauge.DataPoints().At(i).Attributes().AsRaw())
+					}
+					data := jsonKVData{
+						"data_points": dataPoints,
+					}
+					if d, err := json.Marshal(data); err == nil {
 						protoContents = append(protoContents, &protocol.Log_Content{
-							Key:   "attributes",
+							Key:   "Gauge",
 							Value: string(d),
 						})
 					}
-				}
+				case pmetric.MetricDataTypeSum:
+					sum := metric.Sum()
 
-				if resourceLog.Resource().Attributes().Len() != 0 {
-					if d, err := json.Marshal(resourceLog.Resource().Attributes().AsRaw()); err == nil {
+					data := jsonKVData{
+						"is_monotonic":            sum.IsMonotonic(),
+						"aggregation_temporality": sum.AggregationTemporality().String(),
+					}
+					if sum.DataPoints().Len() > 0 {
+						dataPoints := jsonArrayData{}
+						for l := 0; l < sum.DataPoints().Len(); l++ {
+							dataPoints = append(dataPoints, sum.DataPoints().At(i).Attributes().AsRaw())
+						}
+						data["data_points"] = dataPoints
+					}
+
+					if d, err := json.Marshal(data); err == nil {
 						protoContents = append(protoContents, &protocol.Log_Content{
-							Key:   "resources",
+							Key:   "Sum",
+							Value: string(d),
+						})
+					}
+				case pmetric.MetricDataTypeHistogram:
+					histogram := metric.Histogram()
+					dataPoints := jsonArrayData{}
+					for l := 0; l < histogram.DataPoints().Len(); l++ {
+						dataPoints = append(dataPoints, histogram.DataPoints().At(i).Attributes().AsRaw())
+					}
+					data := jsonKVData{
+						"data_points": dataPoints,
+					}
+					if d, err := json.Marshal(data); err == nil {
+						protoContents = append(protoContents, &protocol.Log_Content{
+							Key:   "Histogram",
+							Value: string(d),
+						})
+					}
+				case pmetric.MetricDataTypeExponentialHistogram:
+					exponentialHistogram := metric.ExponentialHistogram()
+					dataPoints := jsonArrayData{}
+					for l := 0; l < exponentialHistogram.DataPoints().Len(); l++ {
+						dataPoints = append(dataPoints, exponentialHistogram.DataPoints().At(i).Attributes().AsRaw())
+					}
+					data := jsonKVData{
+						"data_points": dataPoints,
+					}
+					if d, err := json.Marshal(data); err == nil {
+						protoContents = append(protoContents, &protocol.Log_Content{
+							Key:   "ExponentialHistogram",
+							Value: string(d),
+						})
+					}
+				case pmetric.MetricDataTypeSummary:
+					summary := metric.Summary()
+					dataPoints := jsonArrayData{}
+					for l := 0; l < summary.DataPoints().Len(); l++ {
+						dataPoints = append(dataPoints, summary.DataPoints().At(i).Attributes().AsRaw())
+					}
+					data := jsonKVData{
+						"data_points": dataPoints,
+					}
+					if d, err := json.Marshal(data); err == nil {
+						protoContents = append(protoContents, &protocol.Log_Content{
+							Key:   "Summary",
 							Value: string(d),
 						})
 					}
 				}
 
 				protoLog := &protocol.Log{
-					Time:     uint32(logRecord.Timestamp().AsTime().Unix()),
+					Time:     uint32(time.Now().Unix()),
 					Contents: protoContents,
 				}
 				logs = append(logs, protoLog)
