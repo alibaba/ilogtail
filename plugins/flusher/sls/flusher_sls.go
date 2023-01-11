9c3bd2807d554c8583a72d17f82cb514a606fb0b
linrunqi08 <linrunqi08@163.com>
2023-01-11 10:22:01 +0800

    try to fix macos static check 2

--- plugins/flusher/sls/flusher_sls.go
+++ plugins/flusher/sls/flusher_sls.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build linux || windows || darwin
-// +build linux windows darwin
+//go:build linux || windows
+// +build linux windows
 
 package sls
 
