66503cbe8b9a1fac8a5f8134e7663ee97042cc26
guogangping <guogangping@bytedance.com>
2023-01-30 22:28:13 +0800

    feat: add external plugins builder

---
+++ scripts/gen_external_plugins.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# Copyright 2021 iLogtail Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ue
+set -o pipefail
+
+CONFIG_FILE=${1:-${EXTERNAL_PLUGINS_CONFIG_FILE:-external_plugins.yml}}
+MOD_FILE=${2:-${GO_MOD_FILE:-go.mod}}
+ROOT_DIR=${3:-$(cd $(dirname "${BASH_SOURCE[0]}") && cd .. && pwd)}
+
+echo "=============GENERATING EXTERNAL PLUGINS=================="
+echo "config: $CONFIG_FILE"
+echo "modfile: $MOD_FILE"
+echo "root-dir: $ROOT_DIR"
+echo "=========================================================="
+
+if [ "$CONFIG_FILE" == "off" ]; then
+  echo "process disabled, nothing to do"
+  return
+fi
+
+go run -mod=mod "$ROOT_DIR/tools/builder" -root-dir="$ROOT_DIR" -config="$CONFIG_FILE" -modfile="$MOD_FILE" && \
+echo "generating external plugins finished successfully"
