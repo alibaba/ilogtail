e24845d6b93707f553f47a6d4e8d96b8e3b01312
liujiapeng.ljp <liujiapeng.ljp@alibaba-inc.com>
2023-01-31 16:36:31 +0800

    add profile input v2

--- helper/profile/pyroscope/pprof/pprof_test.go
+++ helper/profile/pyroscope/pprof/pprof_test.go
@@ -41,6 +41,7 @@ func TestRawProfile_ParseV2(t *testing.T) {
 		sampleTypesFilter:   filterKnownSamples(DefaultSampleTypeMapping),
 	}
 	r := new(RawProfile)
+	r.group = new(models.PipelineGroupEvents)
 	meta := &profile.Meta{
 		Key:             segment.NewKey(map[string]string{"_app_name_": "12"}),
 		SpyName:         "go",
@@ -54,11 +55,8 @@ func TestRawProfile_ParseV2(t *testing.T) {
 	err = extractLogs(context.Background(), te, p, meta, cb)
 	require.NoError(t, err)
 	group := r.group
-	require.Equal(t, 4, group.Group.Metadata.Len())
+	require.Equal(t, 0, group.Group.Metadata.Len())
 	require.Equal(t, 0, group.Group.Tags.Len())
-	require.Equal(t, "go", group.Group.Metadata.Get("language"))
-	require.Equal(t, "profile_cpu", group.Group.Metadata.Get("type"))
-	require.Equal(t, "CallStack", group.Group.Metadata.Get("dataType"))
 	require.Equal(t, 6, len(group.Events))
 	event := helper.PickEvent(group.Events, "runtime.kevent /opt/homebrew/Cellar/go/1.16.1/libexec/src/runtime/sys_darwin.go")
 	require.True(t, event != nil)
@@ -66,9 +64,12 @@ func TestRawProfile_ParseV2(t *testing.T) {
 
 	require.Equal(t, "runtime.kevent /opt/homebrew/Cellar/go/1.16.1/libexec/src/runtime/sys_darwin.go", m.Name)
 	require.Equal(t, models.ProfileStack(strings.Split("runtime.netpoll /opt/homebrew/Cellar/go/1.16.1/libexec/src/runtime/netpoll_kqueue.go\nruntime.findrunnable /opt/homebrew/Cellar/go/1.16.1/libexec/src/runtime/proc.go\nruntime.schedule /opt/homebrew/Cellar/go/1.16.1/libexec/src/runtime/proc.go\nruntime.park_m /opt/homebrew/Cellar/go/1.16.1/libexec/src/runtime/proc.go\nruntime.mcall /opt/homebrew/Cellar/go/1.16.1/libexec/src/runtime/asm_arm64.s", "\n")), m.Stack)
-	require.Equal(t, "4682452006970879243", m.StackID)
+	require.Equal(t, "40fb694aa9506d0b", m.StackID)
 	require.Equal(t, int64(1619321948265140000), m.StartTime)
 	require.Equal(t, int64(1619321949365317167), m.EndTime)
+	require.Equal(t, "go", m.Language)
+	require.Equal(t, "profile_cpu", m.ProfileType.String())
+	require.Equal(t, "CallStack", m.DataType)
 	require.Equal(t, models.NewTagsWithMap(map[string]string{
 		"_app_name_": "12",
 	}), m.Tags)
