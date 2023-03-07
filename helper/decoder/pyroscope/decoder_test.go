74778014d6128da725068c8135afa4893d1adf9f
Evan <31562192+EvanLjp@users.noreply.github.com>
2023-03-07 11:56:35 +0800

    unify-profile-cpu-unit (#678)
    * unify-profile-cpu-unit
    
    * fix jfr unit
    
    * fix cpu and wall
    
    ---------
    
    Co-authored-by: liujiapeng.ljp <liujiapeng.ljp@alibaba-inc.com>
    Co-authored-by: linrunqi08 <linrunqi08@163.com>

--- helper/decoder/pyroscope/decoder_test.go
+++ helper/decoder/pyroscope/decoder_test.go
@@ -2,9 +2,14 @@ package pyroscope
 
 import (
 	"bytes"
+	"encoding/binary"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"sort"
 	"testing"
 
+	"github.com/alibaba/ilogtail/helper"
 	"github.com/alibaba/ilogtail/plugins/test"
 
 	"github.com/pyroscope-io/pyroscope/pkg/structs/transporttrie"
@@ -53,11 +58,47 @@ func TestDecoder_DecodeTire(t *testing.T) {
 	require.Equal(t, test.ReadLogVal(log, "stack"), "bar\nfoo")
 	require.Equal(t, test.ReadLogVal(log, "language"), "ebpf")
 	require.Equal(t, test.ReadLogVal(log, "type"), "profile_cpu")
-	require.Equal(t, test.ReadLogVal(log, "units"), "samples")
+	require.Equal(t, test.ReadLogVal(log, "units"), "nanoseconds")
 	require.Equal(t, test.ReadLogVal(log, "valueTypes"), "cpu")
 	require.Equal(t, test.ReadLogVal(log, "aggTypes"), "sum")
 	require.Equal(t, test.ReadLogVal(log, "dataType"), "CallStack")
 	require.Equal(t, test.ReadLogVal(log, "durationNs"), "10000000000")
 	require.Equal(t, test.ReadLogVal(log, "labels"), "{\"__name__\":\"demo\",\"a\":\"b\",\"cluster\":\"sls-mall\"}")
-	require.Equal(t, test.ReadLogVal(log, "val"), "1.00")
+	require.Equal(t, test.ReadLogVal(log, "val"), "10000000.00")
+}
+
+func TestDecoder_DecodePprofCumulative(t *testing.T) {
+	data, err := ioutil.ReadFile("test/dump_pprof_mem_data")
+	require.NoError(t, err)
+	var length uint32
+	buffer := bytes.NewBuffer(data)
+	require.NoError(t, binary.Read(buffer, binary.BigEndian, &length))
+	data = data[4 : 4+int(length)]
+	var d helper.DumpData
+	require.NoError(t, json.Unmarshal(data, &d))
+	request, err := http.NewRequest("POST", d.Req.URL, bytes.NewReader(d.Req.Body))
+	require.NoError(t, err)
+	request.Header = d.Req.Header
+	dec := new(Decoder)
+	logs, err := dec.Decode(d.Req.Body, request, map[string]string{"cluster": "sls-mall"})
+	require.NoError(t, err)
+	require.Equal(t, len(logs), 4)
+	sort.Slice(logs, func(i, j int) bool {
+		return test.ReadLogVal(logs[i], "name") < test.ReadLogVal(logs[j], "name") || test.ReadLogVal(logs[i], "valueTypes") < test.ReadLogVal(logs[j], "valueTypes")
+	})
+	require.Equal(t, test.ReadLogVal(logs[0], "name"), "compress/flate.NewWriter /Users/evan/sdk/go1.19.4/src/compress/flate/deflate.go")
+	require.Equal(t, test.ReadLogVal(logs[0], "valueTypes"), "alloc_objects")
+	require.Equal(t, test.ReadLogVal(logs[0], "val"), "1.00")
+
+	require.Equal(t, test.ReadLogVal(logs[1], "name"), "compress/flate.NewWriter /Users/evan/sdk/go1.19.4/src/compress/flate/deflate.go")
+	require.Equal(t, test.ReadLogVal(logs[1], "valueTypes"), "alloc_space")
+	require.Equal(t, test.ReadLogVal(logs[1], "val"), "924248.00")
+
+	require.Equal(t, test.ReadLogVal(logs[2], "name"), "runtime/pprof.WithLabels /Users/evan/sdk/go1.19.4/src/runtime/pprof/label.go")
+	require.Equal(t, test.ReadLogVal(logs[2], "valueTypes"), "alloc_objects")
+	require.Equal(t, test.ReadLogVal(logs[2], "val"), "1820.00")
+
+	require.Equal(t, test.ReadLogVal(logs[3], "name"), "runtime/pprof.WithLabels /Users/evan/sdk/go1.19.4/src/runtime/pprof/label.go")
+	require.Equal(t, test.ReadLogVal(logs[3], "valueTypes"), "alloc_space")
+	require.Equal(t, test.ReadLogVal(logs[3], "val"), "524432.00")
 }
