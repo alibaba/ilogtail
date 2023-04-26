c3f566d5815c9e37c528735dc930a7140dee503d
yangyunlong.2022 <yangyunlong.2022@bytedance.com>
2023-04-26 23:41:52 +0800

    add BaseVersion

--- scripts/windows64_build.bat
+++ scripts/windows64_build.bat
@@ -6,7 +6,11 @@ REM 2. Build iLogtail.
 REM 3. Build iLogtail plugin.
 REM 4. Make package.
 
-
+set ILOGTAIL_VERSION=%1
+if "%ILOGTAIL_VERSION%" == "" (
+    echo Must specify iLogtail version.
+    goto quit
+)
 set CurrentPath=%~dp0
 set P1Path=
 set P2Path=
@@ -72,7 +76,8 @@ echo Begin to build plugins...
 IF exist %OUTPUT_DIR% ( rd /s /q %OUTPUT_DIR% )
 mkdir %OUTPUT_DIR%
 xcopy /Y %ILOGTAIL_CORE_BUILD_PATH%\plugin\Release\PluginAdapter.dll %ILOGTAIL_PLUGIN_SRC_PATH%\pkg\logtail
-go build -mod=mod -buildmode=c-shared -o %OUTPUT_DIR%\PluginBase.dll %ILOGTAIL_PLUGIN_SRC_PATH%/plugin_main
+set LDFLAGS="-X "github.com/alibaba/ilogtail/pluginmanager.BaseVersion=%LOGTAIL_VERSION%""
+go build -mod=mod -buildmode=c-shared -ldflags=%LDFLAGS% -o output\PluginBase.dll %LOGTAIL_PLUGIN_SRC_PATH%/plugin_main
 if not %ERRORLEVEL% == 0 (
     echo Build iLogtail plugin source failed.
     goto quit
