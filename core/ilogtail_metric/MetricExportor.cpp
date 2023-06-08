0b9161ad3c417ba13110b869402f2c515d1ee896
linrunqi08 <linrunqi08@163.com>
2023-06-08 11:26:03 +0800

    fix demo

--- core/ilogtail_metric/MetricExportor.cpp
+++ core/ilogtail_metric/MetricExportor.cpp
@@ -40,7 +40,20 @@ void MetricExportor::pushInstanceMetric(bool forceSend) {
     */
     std::unordered_map<std::string, PipelineMetric*> pipeLineMetrics = ILogtailMetric::GetInstance()->mPipelineMetrics;
     for (std::unordered_map<std::string, PipelineMetric*>::iterator iter = pipeLineMetrics.begin(); iter != pipeLineMetrics.end(); ++iter) {
-        LOG_INFO(sLogger, ("pipeline_metric value", iter->first));
+        LOG_INFO(sLogger, ("pipeline_metric key", iter->first));
+        PipelineMetric* pilelineMetric = iter->second;
+        for (std::unordered_map<std::string, PluginMetric*>::iterator  it2 = pilelineMetric->mPluginMetrics.begin(); it2 != pilelineMetric->mPluginMetrics.end(); ++ it2) {
+            LOG_INFO(sLogger, ("plugin_metric key", it2->first));
+            PluginMetric* pluginMetric = it2 -> second;
+            for (std::unordered_map<std::string, SubPluginMetric*>::iterator  it3 = pluginMetric->mSubMetrics.begin(); it3 != pluginMetric->mSubMetrics.end(); ++ it3) {
+                LOG_INFO(sLogger, ("sub_metric key", it3->first));
+                SubPluginMetric* subMetric = it3 -> second;
+                for ( std::unordered_map<std::string, BaseMetric*>::iterator it4 = subMetric->mBaseMetrics.begin(); it4 != subMetric->mBaseMetrics.end(); ++ it4) {
+                    LOG_INFO(sLogger, ("base_metric key", it4->first));
+                    LOG_INFO(sLogger, ("base_metric val", it4->second->getMetricObj()->val));
+                }
+            }
+        }
     }
     mLastSendTime = curTime;
 }
