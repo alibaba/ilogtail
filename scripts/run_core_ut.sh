d76e5c75a1964c54467563dca121f3e53e08825c
Abingcbc <abingcbc626@gmail.com>
2023-07-04 20:03:52 +0800

    test cache

--- scripts/run_core_ut.sh
+++ scripts/run_core_ut.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# set -e
+set -e
 
 TARGET_ARTIFACT_PATH=${TARGET_ARTIFACT_PATH:-"./core/build/unittest"}
 
