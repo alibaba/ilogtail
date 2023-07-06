b768fe4170b7a7d2190fc0118d3b88806ab0232c
quzard <1191890118@qq.com>
2023-07-06 15:59:30 +0800

    change package name

--- plugins/processor/sls_metric/processor_sls_metric_test.go
+++ plugins/processor/sls_metric/processor_sls_metric_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package metric
+package sls_metric
 
 import (
 	"github.com/alibaba/ilogtail/pkg/protocol"
