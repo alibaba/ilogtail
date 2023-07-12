181523f98855e7af2c7f6a20e731770d0eb5eec0
quzard <1191890118@qq.com>
2023-07-12 10:52:05 +0800

    change package name

--- plugins/processor/logtoslsmetric/processor_log_to_sls_metric_test.go
+++ plugins/processor/logtoslsmetric/processor_log_to_sls_metric_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package log_to_sls_metric
+package logtoslsmetric
 
 import (
 	"testing"
