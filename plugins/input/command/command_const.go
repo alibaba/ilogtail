1b60fc3bd1147fb561c4a65f07b4ae26046fe53b
quzard <1191890118@qq.com>
2023-07-17 09:46:55 +0800

    Change to english comment

--- plugins/input/command/command_const.go
+++ plugins/input/command/command_const.go
@@ -1,12 +1,10 @@
 package command
 
 const (
-	pluginName              = "metric_command"
+	pluginName              = "input_command"
 	defaultContentType      = "PlainText"
-	defaultLineSplitSep     = "\n"
-	defaultIntervalMs       = 5000 //ms 3默认为阿里云的采集频率5s
-	defaltExecScriptTimeOut = 3000 //单位ms 设置为3秒超时
-	ScriptMd5               = "script_md5"
+	defaultIntervalMs       = 5000 // The default is Alibaba Cloud's collection frequency of 5s
+	defaltExecScriptTimeOut = 3000 // Default 3 seconds timeout
 	UserRoot                = "root"
 	ContentTypeBase64       = "Base64"
 	ContentTypePlainText    = "PlainText"
@@ -17,7 +15,7 @@ type ScriptMeta struct {
 	defaultCmdPath string
 }
 
-// 支持的脚本类型
+// ScriptTypeToSuffix Supported script types
 var ScriptTypeToSuffix = map[string]ScriptMeta{
 	"bash": {
 		scriptSuffix:   "sh",
@@ -37,7 +35,7 @@ var ScriptTypeToSuffix = map[string]ScriptMeta{
 	},
 }
 
-// 支持的contentType
+// SupportContentType Supported content types
 var SupportContentType = map[string]bool{
 	ContentTypePlainText: true,
 	ContentTypeBase64:    true,
