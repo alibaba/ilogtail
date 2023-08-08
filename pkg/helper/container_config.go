4b1b7ecb625bff7dd392eb6224e4b4f5cc09582b
linrunqi08 <linrunqi08@163.com>
2023-08-08 10:47:43 +0800

    refine code

--- pkg/helper/container_config.go
+++ pkg/helper/container_config.go
@@ -244,26 +244,3 @@ func GetShortID(fullID string) string {
 func GetStringFromList(list []string) string {
 	return strings.Join(list, ";")
 }
-
-func SameStringSlice(x, y []string) bool {
-	if len(x) != len(y) {
-		return false
-	}
-	// create a map of string -> int
-	diff := make(map[string]int, len(x))
-	for _, _x := range x {
-		// 0 value for int is 0, so just increment a counter for the string
-		diff[_x]++
-	}
-	for _, _y := range y {
-		// If the string _y is not in diff bail out early
-		if _, ok := diff[_y]; !ok {
-			return false
-		}
-		diff[_y] -= 1
-		if diff[_y] == 0 {
-			delete(diff, _y)
-		}
-	}
-	return len(diff) == 0
-}
