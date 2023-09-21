c1a9cdb283c0284697b13166968c81d39a2acb13
Takuka0311 <1914426213@qq.com>
2023-09-21 11:23:18 +0000

    Annotate the init code

--- core/logtail_windows.cpp
+++ core/logtail_windows.cpp
@@ -179,7 +179,7 @@ void do_worker_process() {
     }
 
     CheckPointManager::Instance()->LoadCheckPoint();
-    AdhocCheckpointManager::GetInstance()->LoadAdhocCheckpoint();
+    // AdhocCheckpointManager::GetInstance()->LoadAdhocCheckpoint();
 
     // added by xianzhi(bowen.gbw@antfin.com)
     // read local data_integrity json file and line count file
