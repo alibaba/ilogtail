286e2814e977a732b3f9366a5b1513d3483f392c
Evan <31562192+EvanLjp@users.noreply.github.com>
2023-09-25 11:29:42 +0800

    Unify metrics format (#1060)
    * temp
    
    * polish metrics log in sls & add pushgateway decoder
    
    * polish metrics log in sls & add pushgateway decoder
    
    * polish codes
    
    * polish codes
    
    * polish codes
    
    * polish codes
    
    * polish codes
    
    * polish codes
    
    * polish codes
    
    * polish codes
    
    * polish codes
    
    * fix lint
    
    * polish codes
    
    * polish codes
    
    * polish codes
    
    * polish codes
    
    * polish codes
    
    * fix unittest

--- pkg/helper/log_helper.go
+++ pkg/helper/log_helper.go
@@ -16,12 +16,25 @@ package helper
 
 import (
 	"fmt"
+	"math"
 	"sort"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/alibaba/ilogtail/pkg/config"
 	"github.com/alibaba/ilogtail/pkg/protocol"
+	"github.com/alibaba/ilogtail/pkg/util"
+)
+
+const (
+	// StaleNaN is a signaling NaN, due to the MSB of the mantissa being 0.
+	// This value is chosen with many leading 0s, so we have scope to store more
+	// complicated values in the future. It is 2 rather than 1 to make
+	// it easier to distinguish from the NormalNaN by a human when debugging.
+	StaleNaN                              uint64 = 0x7ff0000000000002
+	StaleNan                                     = "__STALE_NAN__"
+	SlsMetricstoreInvalidReplaceCharacter        = '_'
 )
 
 func CreateLog(t time.Time, configTag map[string]string, logTags map[string]string, fields map[string]string) (*protocol.Log, error) {
@@ -96,25 +109,112 @@ type MetricLabel struct {
 }
 
 // Labels for metric labels
-type MetricLabels []MetricLabel
-
-func (l MetricLabels) Len() int {
-	return len(l)
+type MetricLabels struct {
+	keyValues []*MetricLabel
+	sorted    bool
+	formatStr string
 }
 
-func (l MetricLabels) Swap(i int, j int) {
-	l[i], l[j] = l[j], l[i]
+func (kv *MetricLabels) clearCache() {
+	kv.sorted = false
+	kv.formatStr = ""
 }
 
-func (l MetricLabels) Less(i int, j int) bool {
-	return l[i].Name < l[j].Name
+func (kv *MetricLabels) Len() int {
+	return len(kv.keyValues)
 }
 
-func MinInt(a, b int) int {
-	if a < b {
-		return a
+func (kv *MetricLabels) Swap(i int, j int) {
+	kv.keyValues[i], kv.keyValues[j] = kv.keyValues[j], kv.keyValues[i]
+}
+
+func (kv *MetricLabels) Less(i int, j int) bool {
+	return kv.keyValues[i].Name < kv.keyValues[j].Name
+}
+
+func (kv *MetricLabels) Replace(key, value string) {
+	findIndex := sort.Search(len(kv.keyValues), func(index int) bool {
+		return kv.keyValues[index].Name >= key
+	})
+	if findIndex < len(kv.keyValues) && kv.keyValues[findIndex].Name == key {
+		kv.keyValues[findIndex].Value = value
+	} else {
+		kv.Append(key, value)
 	}
-	return b
+	kv.clearCache()
+}
+
+func (kv *MetricLabels) Clone() *MetricLabels {
+	if kv == nil {
+		return &MetricLabels{}
+	}
+	var newKeyValues MetricLabels
+	kv.CloneInto(&newKeyValues)
+	return &newKeyValues
+}
+
+func (kv *MetricLabels) CloneInto(dst *MetricLabels) *MetricLabels {
+	if kv == nil {
+		return &MetricLabels{}
+	}
+	if dst == nil {
+		return kv.Clone()
+	}
+	if len(kv.keyValues) < cap(dst.keyValues) {
+		dst.keyValues = dst.keyValues[:len(kv.keyValues)]
+	} else {
+		dst.keyValues = make([]*MetricLabel, len(kv.keyValues))
+	}
+	dst.sorted = kv.sorted
+	dst.formatStr = kv.formatStr
+	for i, value := range kv.keyValues {
+		cp := *value
+		dst.keyValues[i] = &cp
+	}
+	return dst
+}
+
+// AppendMap ...
+func (kv *MetricLabels) AppendMap(mapVal map[string]string) {
+	for key, value := range mapVal {
+		kv.Append(key, value)
+	}
+	kv.clearCache()
+}
+
+// Append ...
+func (kv *MetricLabels) Append(key, value string) {
+	kv.keyValues = append(kv.keyValues, &MetricLabel{
+		formatLabelKey(key),
+		formatLabelValue(value),
+	})
+	kv.clearCache()
+}
+
+func (kv *MetricLabels) SubSlice(begin, end int) {
+	kv.keyValues = kv.keyValues[begin:end]
+	kv.clearCache()
+}
+
+func (kv *MetricLabels) String() string {
+	if kv == nil {
+		return ""
+	}
+	if !kv.sorted || kv.formatStr == "" {
+		sort.Sort(kv)
+		var builder strings.Builder
+		for index, label := range kv.keyValues {
+			builder.WriteString(label.Name)
+			builder.WriteString("#$#")
+			builder.WriteString(label.Value)
+			if index != len(kv.keyValues)-1 {
+				builder.WriteByte('|')
+			}
+		}
+		kv.formatStr = builder.String()
+		kv.sorted = true
+	}
+	return kv.formatStr
 }
 
 // DefBucket ...
@@ -131,42 +231,128 @@ type HistogramData struct {
 }
 
 // ToMetricLogs ..
-func (hd *HistogramData) ToMetricLogs(name string, timeMs int64, labels MetricLabels) []*protocol.Log {
+func (hd *HistogramData) ToMetricLogs(name string, timeMs int64, labels *MetricLabels) []*protocol.Log {
 	logs := make([]*protocol.Log, 0, len(hd.Buckets)+2)
-	sort.Sort(labels)
+	logs = append(logs, NewMetricLog(name+"_count", timeMs, float64(hd.Count), labels))
+	logs = append(logs, NewMetricLog(name+"_sum", timeMs, hd.Sum, labels))
 	for _, v := range hd.Buckets {
-		newLabels := make(MetricLabels, len(labels), len(labels)+1)
-		copy(newLabels, labels)
-		newLabels = append(newLabels, MetricLabel{Name: "le", Value: strconv.FormatFloat(v.Le, 'g', -1, 64)})
-		sort.Sort(newLabels)
-		logs = append(logs, NewMetricLog(name+"_bucket", timeMs, strconv.FormatInt(v.Count, 10), newLabels))
+		labels.Replace("le", strconv.FormatFloat(v.Le, 'g', -1, 64))
+		logs = append(logs, NewMetricLog(name+"_bucket", timeMs, float64(v.Count), labels))
 	}
-	logs = append(logs, NewMetricLog(name+"_count", timeMs, strconv.FormatInt(hd.Count, 10), labels))
-	logs = append(logs, NewMetricLog(name+"_sum", timeMs, strconv.FormatFloat(hd.Sum, 'g', -1, 64), labels))
+
 	return logs
 }
 
-// NewMetricLog caller must sort labels
-func NewMetricLog(name string, timeMs int64, value string, labels []MetricLabel) *protocol.Log {
-	strTime := strconv.FormatInt(timeMs, 10)
-	metric := &protocol.Log{}
-	protocol.SetLogTimeWithNano(metric, uint32(timeMs/1000), uint32((timeMs*1e6)%1e9))
-	metric.Contents = []*protocol.Log_Content{}
-	metric.Contents = append(metric.Contents, &protocol.Log_Content{Key: "__name__", Value: name})
-	metric.Contents = append(metric.Contents, &protocol.Log_Content{Key: "__time_nano__", Value: strTime})
-
-	builder := strings.Builder{}
-	for index, l := range labels {
-		if index != 0 {
-			builder.WriteString("|")
-		}
-		builder.WriteString(l.Name)
-		builder.WriteString("#$#")
-		builder.WriteString(l.Value)
-
+// NewMetricLog create a metric log, time support unix milliseconds and unix nanoseconds.
+func NewMetricLog(name string, t int64, value float64, labels *MetricLabels) *protocol.Log {
+	var valStr string
+	if math.Float64bits(value) == StaleNaN {
+		valStr = StaleNan
+	} else {
+		valStr = strconv.FormatFloat(value, 'g', -1, 64)
 	}
-	metric.Contents = append(metric.Contents, &protocol.Log_Content{Key: "__labels__", Value: builder.String()})
+	return NewMetricLogStringVal(name, t, valStr, labels)
+}
 
+// NewMetricLogStringVal create a metric log with val string, time support unix milliseconds and unix nanoseconds.
+func NewMetricLogStringVal(name string, t int64, value string, labels *MetricLabels) *protocol.Log {
+	strTime := strconv.FormatInt(t, 10)
+	metric := &protocol.Log{}
+	switch len(strTime) {
+	case 13:
+		protocol.SetLogTimeWithNano(metric, uint32(t/1000), uint32((t*1e6)%1e9))
+		strTime += "000000"
+	case 19:
+		protocol.SetLogTimeWithNano(metric, uint32(t/1e9), uint32(t%1e9))
+	default:
+		t = int64(float64(t) * math.Pow10(19-len(strTime)))
+		strTime = strconv.FormatInt(t, 10)
+		protocol.SetLogTimeWithNano(metric, uint32(t/1e9), uint32(t%1e9))
+	}
+	metric.Contents = make([]*protocol.Log_Content, 0, 4)
+	metric.Contents = append(metric.Contents, &protocol.Log_Content{Key: "__name__", Value: formatNewMetricName(name)})
+	metric.Contents = append(metric.Contents, &protocol.Log_Content{Key: "__time_nano__", Value: strTime})
+	metric.Contents = append(metric.Contents, &protocol.Log_Content{Key: "__labels__", Value: labels.String()})
 	metric.Contents = append(metric.Contents, &protocol.Log_Content{Key: "__value__", Value: value})
 	return metric
+}
+
+func formatLabelKey(key string) string {
+	if !config.LogtailGlobalConfig.EnableSlsMetricsFormat {
+		return key
+	}
+	var newKey []byte
+	for i := 0; i < len(key); i++ {
+		b := key[i]
+		if (b >= 'a' && b <= 'z') ||
+			(b >= 'A' && b <= 'Z') ||
+			(b >= '0' && b <= '9') ||
+			b == '_' {
+			continue
+		} else {
+			if newKey == nil {
+				newKey = []byte(key)
+			}
+			newKey[i] = SlsMetricstoreInvalidReplaceCharacter
+		}
+	}
+	if newKey == nil {
+		return key
+	}
+	return util.ZeroCopyBytesToString(newKey)
+}
+
+func formatLabelValue(value string) string {
+	if !config.LogtailGlobalConfig.EnableSlsMetricsFormat {
+		return value
+	}
+	var newValue []byte
+	for i := 0; i < len(value); i++ {
+		b := value[i]
+		if b != '|' {
+			continue
+		} else {
+			if newValue == nil {
+				newValue = []byte(value)
+			}
+			newValue[i] = SlsMetricstoreInvalidReplaceCharacter
+		}
+	}
+	if newValue == nil {
+		return value
+	}
+	return util.ZeroCopyBytesToString(newValue)
+}
+
+func formatNewMetricName(name string) string {
+	if !config.LogtailGlobalConfig.EnableSlsMetricsFormat {
+		return name
+	}
+	newName := []byte(name)
+	for i, b := range newName {
+		if (b >= 'a' && b <= 'z') ||
+			(b >= 'A' && b <= 'Z') ||
+			(b >= '0' && b <= '9') ||
+			b == '_' ||
+			b == ':' {
+			continue
+		} else {
+			newName[i] = SlsMetricstoreInvalidReplaceCharacter
+		}
+	}
+	return util.ZeroCopyBytesToString(newName)
+}
+
+// ReplaceInvalidChars analog of invalidChars = regexp.MustCompile("[^a-zA-Z0-9_]")
+func ReplaceInvalidChars(in *string) {
+	for charIndex, char := range *in {
+		charInt := int(char)
+		if !((charInt >= 97 && charInt <= 122) || // a-z
+			(charInt >= 65 && charInt <= 90) || // A-Z
+			(charInt >= 48 && charInt <= 57) || // 0-9
+			charInt == 95 || charInt == ':') { // _
+
+			*in = (*in)[:charIndex] + "_" + (*in)[charIndex+1:]
+		}
+	}
 }
--- plugins/input/skywalkingv2/jvm_metric_handle.go
+++ plugins/input/skywalkingv2/jvm_metric_handle.go
@@ -16,7 +16,6 @@ package skywalkingv2
 
 import (
 	"context"
-	"strconv"
 
 	"github.com/alibaba/ilogtail/pkg/helper"
 	"github.com/alibaba/ilogtail/pkg/pipeline"
@@ -51,10 +50,15 @@ func (j *JVMMetricServiceHandle) Collect(ctx context.Context, metrics *agent.JVM
 
 func toMetricStoreFormat(metric *agent.JVMMetric, service string, serviceInstance string, host string) []*protocol.Log {
 	var logs []*protocol.Log
-	cpuUsage := helper.NewMetricLog("skywalking_jvm_cpu_usage", metric.Time,
-		strconv.FormatFloat(metric.GetCpu().UsagePercent, 'f', 6, 64), []helper.MetricLabel{{Name: "host", Value: host}, {Name: "service", Value: service}, {Name: "serviceInstance", Value: serviceInstance}})
+	var labels helper.MetricLabels
+	labels.Append("service", service)
+	labels.Append("serviceInstance", serviceInstance)
+	labels.Append("host", host)
+
+	cpuUsage := helper.NewMetricLog("skywalking_jvm_cpu_usage", metric.Time, metric.GetCpu().UsagePercent, &labels)
 	logs = append(logs, cpuUsage)
 
+	memLabels := labels.Clone()
 	for _, mem := range metric.Memory {
 		var memType string
 		if mem.IsHeap {
@@ -62,63 +66,50 @@ func toMetricStoreFormat(metric *agent.JVMMetric, service string, serviceInstanc
 		} else {
 			memType = "nonheap"
 		}
-		memCommitted := helper.NewMetricLog("skywalking_jvm_memory_committed", metric.GetTime(),
-			strconv.FormatInt(mem.Committed, 10),
-			[]helper.MetricLabel{{Name: "host", Value: host}, {Name: "service", Value: service}, {Name: "serviceInstance", Value: serviceInstance}, {Name: "type", Value: memType}})
+		memLabels.Replace("type", memType)
+
+		memCommitted := helper.NewMetricLog("skywalking_jvm_memory_committed", metric.GetTime(), float64(mem.Committed), memLabels)
 		logs = append(logs, memCommitted)
 
-		memInit := helper.NewMetricLog("skywalking_jvm_memory_init", metric.GetTime(),
-			strconv.FormatInt(mem.Init, 10),
-			[]helper.MetricLabel{{Name: "host", Value: host}, {Name: "service", Value: service}, {Name: "serviceInstance", Value: serviceInstance}, {Name: "type", Value: memType}})
+		memInit := helper.NewMetricLog("skywalking_jvm_memory_init", metric.GetTime(), float64(mem.Init), memLabels)
 		logs = append(logs, memInit)
 
-		memMax := helper.NewMetricLog("skywalking_jvm_memory_max", metric.GetTime(),
-			strconv.FormatInt(mem.Max, 10),
-			[]helper.MetricLabel{{Name: "host", Value: host}, {Name: "service", Value: service}, {Name: "serviceInstance", Value: serviceInstance}, {Name: "type", Value: memType}})
+		memMax := helper.NewMetricLog("skywalking_jvm_memory_max", metric.GetTime(), float64(mem.Max), memLabels)
 		logs = append(logs, memMax)
 
-		memUsed := helper.NewMetricLog("skywalking_jvm_memory_used", metric.GetTime(),
-			strconv.FormatInt(mem.Used, 10),
-			[]helper.MetricLabel{{Name: "host", Value: host}, {Name: "service", Value: service}, {Name: "serviceInstance", Value: serviceInstance}, {Name: "type", Value: memType}})
+		memUsed := helper.NewMetricLog("skywalking_jvm_memory_used", metric.GetTime(), float64(mem.Used), memLabels)
 		logs = append(logs, memUsed)
 	}
+
+	for _, memPool := range metric.MemoryPool {
+		memLabels.Replace("type", memPool.Type.String())
+		memPoolCommitted := helper.NewMetricLog("skywalking_jvm_memory_pool_committed", metric.GetTime(), float64(memPool.Commited), memLabels)
+		logs = append(logs, memPoolCommitted)
+
+		memPoolInit := helper.NewMetricLog("skywalking_jvm_memory_pool_init", metric.GetTime(), float64(memPool.Init), memLabels)
+		logs = append(logs, memPoolInit)
+
+		memPoolMax := helper.NewMetricLog("skywalking_jvm_memory_pool_max", metric.GetTime(), float64(memPool.Max), memLabels)
+		logs = append(logs, memPoolMax)
+
+		memPoolUsed := helper.NewMetricLog("skywalking_jvm_memory_pool_used", metric.GetTime(), float64(memPool.Used), memLabels)
+		logs = append(logs, memPoolUsed)
+	}
+
+	gcLabels := labels.CloneInto(memLabels)
 	for _, gc := range metric.Gc {
-		gcTime := helper.NewMetricLog("skywalking_jvm_gc_time", metric.GetTime(),
-			strconv.FormatInt(gc.Time, 10),
-			[]helper.MetricLabel{{Name: "host", Value: host}, {Name: "phrase", Value: gc.Phrase.String()}, {Name: "service", Value: service}, {Name: "serviceInstance", Value: serviceInstance}})
+		memLabels.Replace("phrase", gc.Phrase.String())
+		gcTime := helper.NewMetricLog("skywalking_jvm_gc_time", metric.GetTime(), float64(gc.Time), gcLabels)
 		logs = append(logs, gcTime)
 
 		phrase := "Old"
 		if gc.Phrase.Number() == agent.GCPhrase_NEW.Number() {
 			phrase = "Young"
 		}
+		memLabels.Replace("phrase", phrase)
 
-		gcCount := helper.NewMetricLog("skywalking_jvm_gc_count", metric.GetTime(),
-			strconv.FormatInt(gc.Count, 10),
-			[]helper.MetricLabel{{Name: "host", Value: host}, {Name: "phrase", Value: phrase}, {Name: "service", Value: service}, {Name: "serviceInstance", Value: serviceInstance}})
+		gcCount := helper.NewMetricLog("skywalking_jvm_gc_count", metric.GetTime(), float64(gc.Count), gcLabels)
 		logs = append(logs, gcCount)
-	}
-
-	for _, memPool := range metric.MemoryPool {
-		memPoolCommitted := helper.NewMetricLog("skywalking_jvm_memory_pool_committed", metric.GetTime(),
-			strconv.FormatInt(memPool.Commited, 10),
-			[]helper.MetricLabel{{Name: "host", Value: host}, {Name: "service", Value: service}, {Name: "serviceInstance", Value: serviceInstance}, {Name: "type", Value: memPool.Type.String()}})
-		logs = append(logs, memPoolCommitted)
-
-		memPoolInit := helper.NewMetricLog("skywalking_jvm_memory_pool_init", metric.GetTime(),
-			strconv.FormatInt(memPool.Init, 10),
-			[]helper.MetricLabel{{Name: "host", Value: host}, {Name: "service", Value: service}, {Name: "serviceInstance", Value: serviceInstance}, {Name: "type", Value: memPool.Type.String()}})
-		logs = append(logs, memPoolInit)
-
-		memPoolMax := helper.NewMetricLog("skywalking_jvm_memory_pool_max", metric.GetTime(),
-			strconv.FormatInt(memPool.Max, 10),
-			[]helper.MetricLabel{{Name: "host", Value: host}, {Name: "service", Value: service}, {Name: "serviceInstance", Value: serviceInstance}, {Name: "type", Value: memPool.Type.String()}})
-		logs = append(logs, memPoolMax)
-
-		memPoolUsed := helper.NewMetricLog("skywalking_jvm_memory_pool_used",
-			metric.GetTime(), strconv.FormatInt(memPool.Used, 10),
-			[]helper.MetricLabel{{Name: "host", Value: host}, {Name: "service", Value: service}, {Name: "serviceInstance", Value: serviceInstance}, {Name: "type", Value: memPool.Type.String()}})
-		logs = append(logs, memPoolUsed)
 	}
 
 	return logs
