04b64adb9e327222752588e70430bd70f307060e
henryzhx8 <zhhxjack8@aliyun.com>
2023-10-18 15:16:07 +0000

    polish code

--- core/flusher/FlusherSLS.cpp
+++ core/flusher/FlusherSLS.cpp
@@ -1,3 +1,17 @@
+// Copyright 2023 iLogtail Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "flusher/FlusherSLS.h"
 
 #include "app_config/AppConfig.h"
@@ -12,6 +26,7 @@ DECLARE_FLAG_INT32(batch_send_interval);
 DEFINE_FLAG_BOOL(sls_client_send_compress, "whether compresses the data or not when put data", true);
 
 namespace logtail {
+
 const string FlusherSLS::sName = "flusher_sls";
 const unordered_set<string> FlusherSLS::sNativeParam = {"Project",
                                                         "Logstore",
@@ -181,4 +196,5 @@ void FlusherSLS::GenerateGoPlugin(const Json::Value& config, Json::Value& res) c
         res["flushers"].append(plugin);
     }
 }
+
 } // namespace logtail
--- core/flusher/FlusherSLS.h
+++ core/flusher/FlusherSLS.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 iLogtail Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #pragma once
 
 #include "plugin/interface/Flusher.h"
@@ -12,6 +28,7 @@
 #include "common/LogstoreFeedbackKey.h"
 
 namespace logtail {
+
 class FlusherSLS : public Flusher {
 public:
     enum class CompressType { NONE, LZ4, ZSTD };
@@ -55,4 +72,5 @@ private:
 
     LogstoreFeedBackKey mLogstoreKey;
 };
+
 } // namespace logtail
