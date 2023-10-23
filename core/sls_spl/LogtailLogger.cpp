68959bfc22bc86cc473587a337a8b43ffce54580
linrunqi08 <linrunqi08@163.com>
2023-10-23 14:16:55 +0800

    refine core

--- core/sls_spl/LogtailLogger.cpp
+++ core/sls_spl/LogtailLogger.cpp
@@ -4,7 +4,6 @@
 namespace apsara::sls::spl {
 
 void LogtailLogger::log(const LOG_LEVEL& level, const std::string& file, const int32_t line, const std::string& content) {
-    //LOG(INFO) << "level: " << level << ", file: " << file << ", line: " << line << ", content: " << content;
     LOG_INFO(sLogger, ("level", level)("file", file)("line", line)("content", content));
 }
 
