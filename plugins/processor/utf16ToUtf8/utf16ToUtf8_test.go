7523d0c0b38092f39b743a6698c5068da22ff1d2
quzard <1191890118@qq.com>
2023-11-01 12:10:22 +0000

    add test

--- plugins/processor/utf16ToUtf8/utf16ToUtf8_test.go
+++ plugins/processor/utf16ToUtf8/utf16ToUtf8_test.go
@@ -15,9 +15,120 @@
 package utf16ToUtf8
 
 import (
-	"github.com/alibaba/ilogtail/pkg/logger"
+	"testing"
+
+	"github.com/alibaba/ilogtail/pkg/protocol"
+	"github.com/alibaba/ilogtail/plugins/test/mock"
 )
 
-func init() {
-	logger.InitTestLogger(logger.OptionOpenMemoryReceiver)
+func newProcessor(endian string) (*ProcessorUtf16ToUtf8, error) {
+	ctx := mock.NewEmptyContext("p", "l", "c")
+	processor := &ProcessorUtf16ToUtf8{
+		SourceKey: "content",
+		Endian:    endian,
+	}
+	err := processor.Init(ctx)
+	return processor, err
+}
+
+func TestProcessorUtf16ToUtf8_Init(t *testing.T) {
+	_, err := newProcessor("Little")
+	if err != nil {
+		t.Errorf("Init failed: %s", err)
+	}
+}
+
+func TestProcessorUtf16ToUtf8_Description(t *testing.T) {
+	p, err := newProcessor("Little")
+	if err != nil {
+		t.Errorf("Init failed: %s", err)
+	}
+	expectedDescription := "Parse utf16 logs into utf8."
+	description := p.Description()
+
+	if description != expectedDescription {
+		t.Errorf("Description does not match. Expected: %s, Got: %s", expectedDescription, description)
+	}
+}
+
+func TestProcessorUtf16ToUtf8_ProcessLogs(t *testing.T) {
+	p, err := newProcessor("Little")
+	if err != nil {
+		t.Errorf("Init failed: %s", err)
+	}
+	logArray := []*protocol.Log{
+		{
+			Contents: []*protocol.Log_Content{
+				{
+					Key:   "content",
+					Value: "你好",
+				},
+			},
+		},
+		{
+			Contents: []*protocol.Log_Content{
+				{
+					Key:   "content",
+					Value: "こんにちは",
+				},
+			},
+		},
+	}
+	expectedResult := logArray
+
+	result := p.ProcessLogs(logArray)
+
+	if len(result) != len(expectedResult) {
+		t.Errorf("ProcessLogs result length does not match. Expected: %d, Got: %d", len(expectedResult), len(result))
+	}
+
+	for i := 0; i < len(result); i++ {
+		if result[i].Contents[0].Value != expectedResult[i].Contents[0].Value {
+			t.Errorf("ProcessLogs result does not match. Expected: %s, Got: %s", expectedResult[i].Contents[0].Value, result[i].Contents[0].Value)
+		}
+	}
+}
+
+func TestProcessorUtf16ToUtf8_Little_processLog(t *testing.T) {
+	p, err := newProcessor("Little")
+	if err != nil {
+		t.Errorf("Init failed: %s", err)
+	}
+	log := &protocol.Log{
+		Contents: []*protocol.Log_Content{
+			{
+				Key:   "content",
+				Value: "\x60\x4F\x7D\x59", // UTF16 encoded string: "你好"
+			},
+		},
+	}
+	expectedResult := "你好"
+
+	p.processLog(log)
+
+	if log.Contents[0].Value != expectedResult {
+		t.Errorf("processLog result does not match. Expected: %s, Got: %s", expectedResult, log.Contents[0].Value)
+	}
+}
+
+func TestProcessorUtf16ToUtf8_Big_processLog(t *testing.T) {
+	p, err := newProcessor("Big")
+	if err != nil {
+		t.Errorf("Init failed: %s", err)
+	}
+	log := &protocol.Log{
+		Contents: []*protocol.Log_Content{
+			{
+				Key:   "content",
+				Value: "\x4F\x60\x59\x7D", // UTF16 encoded string: "你好"
+			},
+		},
+	}
+	expectedResult := "你好"
+
+	p.processLog(log)
+
+	if log.Contents[0].Value != expectedResult {
+		t.Errorf("processLog result does not match. Expected: %s, Got: %s", expectedResult, log.Contents[0].Value)
+	}
 }
