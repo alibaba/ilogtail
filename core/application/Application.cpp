209d6c6ca07509b0cdddc1f02bc6c95f1fb99a0b
henryzhx8 <zhhxjack8@aliyun.com>
2023-12-08 02:00:31 +0000

    fix

--- core/application/Application.cpp
+++ core/application/Application.cpp
@@ -215,7 +215,7 @@ void Application::Start() {
     LogProcess::GetInstance()->Start();
 
     time_t curTime = 0, lastProfilingCheckTime = 0, lastTcmallocReleaseMemTime = 0, lastConfigCheckTime = 0,
-           lastUpdateMetricTime = 0, lastCheckTagsTime = 0, lastCheckContainerUpdateTime = 0;
+           lastUpdateMetricTime = 0, lastCheckTagsTime = 0;
     while (true) {
         curTime = time(NULL);
         if (curTime - lastCheckTagsTime >= INT32_FLAG(file_tags_update_interval)) {
@@ -254,13 +254,12 @@ void Application::Start() {
         // 过渡使用
         EventDispatcher::GetInstance()->DumpCheckPointPeriod(curTime);
 
-        if (curTime - lastCheckContainerUpdateTime >= 2) {
-            if (ConfigManager::GetInstance()->IsUpdateContainerPaths()) {
-                FileServer::GetInstance()->Pause();
-                FileServer::GetInstance()->Resume();
-            }
-            lastCheckContainerUpdateTime = curTime;
+        if (ConfigManager::GetInstance()->IsUpdateContainerPaths()) {
+            FileServer::GetInstance()->Pause();
+            FileServer::GetInstance()->Resume();
         }
+
+        this_thread::sleep_for(chrono::seconds(1));
     }
 }
 
