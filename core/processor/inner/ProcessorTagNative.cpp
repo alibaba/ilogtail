5e1ab176db7c119bc57949bfe3a0704973fec8e2
henryzhx8 <zhhxjack8@aliyun.com>
2024-06-14 08:34:21 +0000

    polish

--- core/processor/inner/ProcessorTagNative.cpp
+++ core/processor/inner/ProcessorTagNative.cpp
@@ -24,7 +24,7 @@
 #include "log_pb/sls_logs.pb.h"
 #include "pipeline/Pipeline.h"
 #ifdef __ENTERPRISE__
-#include "enterprise/EnterpriseConfigProvider.h"
+#include "config/provider/EnterpriseConfigProvider.h"
 #endif
 
 DECLARE_FLAG_STRING(ALIYUN_LOG_FILE_TAGS);
