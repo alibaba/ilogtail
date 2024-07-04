b2a48d52e5c83b892a576f3fa2d42a6140570aeb
alph00 <alph000@163.com>
2024-07-04 15:56:52 +0800

    fix link sequence error

--- core/processor/links.cmake
+++ core/processor/links.cmake
@@ -17,6 +17,11 @@
 macro(processor_link target_name)
     link_re2(${target_name})
     if (LINUX AND NOT WITHOUTSPL)
+        link_spl(${target_name})
+    endif ()
+    if (LINUX AND NOT WITHOUTSPL)
         target_link_libraries(${target_name} spl)
     endif ()
+    link_ssl(${target_name}) # must after link_spl
+    link_crypto(${target_name}) # must after link_spl
 endmacro()
