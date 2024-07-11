0abfea46be43a2f15ea6d00ce853550712819b6e
zhushunjia <zhushunjia@bytedance.com>
2024-07-12 00:44:34 +0800

    comments

--- core/provider/provider.h
+++ core/provider/provider.h
@@ -1,13 +1,15 @@
 #pragma once
 
+// #include "app_config/attributes/AttributesProvider.h"
 #include "config/provider/ConfigProvider.h"
 #include "profile_sender/ProfileSender.h"
 
 namespace logtail {
 
+// AttributesProvider* GetAttributesProvider();
 
 ConfigProvider* GetRemoteConfigProvider();
 
-
 ProfileSender* GetProfileSenderProvider();    
-} 
+
+} // namespace logtail 
