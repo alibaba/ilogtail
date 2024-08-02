60aed133dbfd0e796ddb9df6f4f74092a0df6f0e
abingcbc <abingcbc626@gmail.com>
2024-08-02 10:02:39 +0800

    tmp remove get cidr

--- pkg/helper/k8smeta/k8s_meta_http_server.go
+++ pkg/helper/k8smeta/k8s_meta_http_server.go
@@ -11,9 +11,6 @@ import (
 	"time"
 
 	"github.com/alibaba/ilogtail/pkg/logger"
-	cs "github.com/alibabacloud-go/cs-20151215/v5/client"
-	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
-	"github.com/alibabacloud-go/tea/tea"
 	v1 "k8s.io/api/core/v1"
 )
 
@@ -52,7 +49,6 @@ func (m *metadataHandler) K8sServerRun(stopCh <-chan struct{}) error {
 	mux.HandleFunc("/metadata/ip", m.handlePodMeta)
 	mux.HandleFunc("/metadata/containerid", m.handlePodMeta)
 	mux.HandleFunc("/metadata/host", m.handlePodMeta)
-	mux.HandleFunc("/metadata/cidr", m.handleCIDR)
 	server.Handler = mux
 	for {
 		if m.metaManager.ready.Load() {
@@ -101,51 +97,6 @@ func (m *metadataHandler) handlePodMeta(w http.ResponseWriter, r *http.Request) 
 		http.Error(w, "Error writing response: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
-}
-
-func (m *metadataHandler) handleCIDR(w http.ResponseWriter, r *http.Request) {
-	cidr, err := getPodCIDR()
-	if err != nil {
-		http.Error(w, "Error getting pod CIDR: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-	cidrJSON, err := json.Marshal(map[string]string{"cidr": cidr})
-	if err != nil {
-		http.Error(w, "Error converting metadata to JSON: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-	// Set the response content type to application/json
-	w.Header().Set("Content-Type", "application/json")
-	// Write the metadata JSON to the response body
-	_, err = w.Write(cidrJSON)
-	if err != nil {
-		http.Error(w, "Error writing response: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-func getPodCIDR() (string, error) {
-	clusterId := os.Getenv("ALICLOUD_ACK_CLUSTER_ID")
-	if len(clusterId) == 0 {
-		return "", fmt.Errorf("ALICLOUD_ACK_CLUSTER_ID is not set")
-	}
-	config := &openapi.Config{
-		AccessKeyId:     tea.String(os.Getenv("ALICLOUD_ACCESS_KEY_ID")),
-		AccessKeySecret: tea.String(os.Getenv("ALICLOUD_ACCESS_KEY_SECRET")),
-		Endpoint:        tea.String(os.Getenv("ALICLOUD_ENDPOINT")),
-	}
-	client, err := cs.NewClient(config)
-	if err != nil {
-		return "", err
-	}
-	response, err := client.DescribeClusterDetail(&clusterId)
-	if err != nil {
-		return "", err
-	}
-	if response.Body.SubnetCidr == nil {
-		return "172.16.0.0/12", nil
-	}
-	return *response.Body.SubnetCidr, nil
 }
 
 func convertObj2PodMetadata(key string, events []*K8sMetaEvent) map[string]*PodMetadata {
