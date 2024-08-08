9a37306bbae0d14286910fc917e6e55f37e0b699
liqiang <liqiang.x@foxmail.com>
2024-08-08 05:43:03 +0000

    2.25s

--- core/unittest/prometheus/TextParserBenchmark.cpp
+++ core/unittest/prometheus/TextParserBenchmark.cpp
@@ -60,7 +60,7 @@ void TextParserBenchmark::TestParse100M() const {
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
     cout << "elapsed: " << elapsed.count() << " seconds" << endl;
-    // elapsed: 2.67 seconds in release mode
+    // elapsed: 2.25 seconds in release mode
 }
 
 UNIT_TEST_CASE(TextParserBenchmark, TestParse100M)
