71596371e28e9ecd0f0aded63ff9bdb212979076
Abingcbc <abingcbc626@gmail.com>
2024-08-27 15:37:39 +0800

    [backport 1.8] recover readers exactly from checkpoint

--- core/event_handler/EventHandler.cpp
+++ core/event_handler/EventHandler.cpp
@@ -368,7 +368,20 @@ LogFileReaderPtr ModifyHandler::CreateLogFileReaderPtr(
                                                                     mNameReaderMap.size() + 1));
     }
     LogFileReaderPtrArray& readerArray = mNameReaderMap[name];
-    if (readerArray.size() >= pConfig->mAdvancedConfig.mMaxRotateQueueSize) {
+
+    LogFileReaderPtr readerPtr(LogFileReader::CreateLogFileReader(path,
+                                                                  name,
+                                                                  devInode,
+                                                                  readerConfig,
+                                                                  multilineConfig,
+                                                                  discoveryConfig,
+                                                                  exactlyonceConcurrency,
+                                                                  forceBeginingFlag));
+    if (readerPtr.get() == NULL)
+        return LogFileReaderPtr();
+
+    if (readerArray.size() >= readerConfig.first->mRotatorQueueSize
+        && readerPtr->GetIdxInReaderArrayFromLastCpt() == LogFileReader::CHECKPOINT_IDX_OF_NEW_READER_IN_ARRAY) {
         int32_t nowTime = time(NULL);
         if (nowTime - mLastOverflowErrorTime > INT32_FLAG(rotate_overflow_error_interval)) {
             mLastOverflowErrorTime = nowTime;
@@ -393,10 +406,6 @@ LogFileReaderPtr ModifyHandler::CreateLogFileReaderPtr(
              ("start to create log reader, project", pConfig->GetProjectName())("logstore", pConfig->GetCategory())(
                  "config", pConfig->mConfigName)("log reader queue name", PathJoin(path, name))(
                  "file device", ToString(devInode.dev))("file inode", ToString(devInode.inode)));
-
-    LogFileReaderPtr readerPtr(pConfig->CreateLogFileReader(path, name, devInode, forceBeginingFlag));
-    if (readerPtr.get() == NULL)
-        return LogFileReaderPtr();
 
     // new log
     bool backFlag = false;
@@ -441,9 +450,26 @@ LogFileReaderPtr ModifyHandler::CreateLogFileReaderPtr(
         }
     }
 
-    backFlag ? readerArray.push_back(readerPtr) : readerArray.push_front(readerPtr);
+    int32_t idx = readerPtr->GetIdxInReaderArrayFromLastCpt();
+    // new reader
+    if (backFlag) {
+        readerArray.push_back(readerPtr);
+        mDevInodeReaderMap[devInode] = readerPtr;
+        // reader not in reader array
+    } else if (idx == LogFileReader::CHECKPOINT_IDX_OF_NOT_IN_READER_ARRAY) {
+        mRotatorReaderMap[devInode] = readerPtr;
+        // reader in reader array
+    } else if (idx >= 0) {
+        readerArray.push_back(readerPtr);
+        mDevInodeReaderMap[devInode] = readerPtr;
+        std::stable_sort(readerArray.begin(), readerArray.end(), ModifyHandler::CompareReaderByIdxFromCpt);
+    } else {
+        LOG_ERROR(sLogger,
+                  ("unexpected idx", idx)("real log path", readerPtr->GetRealLogPath())("host log path",
+                                                                                        readerPtr->GetHostLogPath()));
+        return LogFileReaderPtr();
+    }
     readerPtr->SetReaderArray(&readerArray);
-    mDevInodeReaderMap[devInode] = readerPtr;
 
     LOG_INFO(
         sLogger,
@@ -932,11 +958,18 @@ void ModifyHandler::HandleTimeOut() {
 bool ModifyHandler::DumpReaderMeta(bool isRotatorReader, bool checkConfigFlag) {
     if (!isRotatorReader) {
         for (DevInodeLogFileReaderMap::iterator it = mDevInodeReaderMap.begin(); it != mDevInodeReaderMap.end(); ++it) {
-            it->second->DumpMetaToMem(checkConfigFlag);
+            int32_t idxInReaderArray = -2;
+            for (size_t i = 0; i < it->second->GetReaderArray()->size(); ++i) {
+                if (it->second->GetReaderArray()->at(i) == it->second) {
+                    idxInReaderArray = i;
+                    break;
+                }
+            }
+            it->second->DumpMetaToMem(checkConfigFlag, idxInReaderArray);
         }
     } else {
         for (DevInodeLogFileReaderMap::iterator it = mRotatorReaderMap.begin(); it != mRotatorReaderMap.end(); ++it) {
-            it->second->DumpMetaToMem(checkConfigFlag);
+            it->second->DumpMetaToMem(checkConfigFlag, -2);
         }
     }
     return true;
