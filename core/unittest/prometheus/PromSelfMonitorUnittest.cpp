a9e7056b8b69198ba359773ef3d6c13e65fd7dfe
liqiang <liqiang.x@foxmail.com>
2024-09-18 03:39:39 +0000

    chore: fix ut

--- core/unittest/prometheus/PromSelfMonitorUnittest.cpp
+++ core/unittest/prometheus/PromSelfMonitorUnittest.cpp
@@ -8,37 +8,24 @@ using namespace std;
 namespace logtail {
 class PromSelfMonitorUnittest : public ::testing::Test {
 public:
-    void TestInitMetricManager();
     void TestCounterAdd();
     void TestIntGaugeSet();
 };
-
-void PromSelfMonitorUnittest::TestInitMetricManager() {
-    auto selfMonitor = std::make_shared<PromSelfMonitor>();
-    std::unordered_map<std::string, MetricType> testMetricKeys = {
-        {PROM_SUBSCRIBE_TARGETS, MetricType::METRIC_TYPE_INT_GAUGE},
-        {PROM_SUBSCRIBE_TOTAL, MetricType::METRIC_TYPE_COUNTER},
-    };
-    selfMonitor->InitMetricManager("test-key", testMetricKeys, MetricLabels{});
-
-    APSARA_TEST_TRUE(selfMonitor->mPromMetricsMap.count("test-key"));
-}
 
 void PromSelfMonitorUnittest::TestCounterAdd() {
     auto selfMonitor = std::make_shared<PromSelfMonitor>();
     std::unordered_map<std::string, MetricType> testMetricKeys = {
         {PROM_SUBSCRIBE_TOTAL, MetricType::METRIC_TYPE_COUNTER},
     };
-    selfMonitor->InitMetricManager("test-key", testMetricKeys, MetricLabels{});
-    APSARA_TEST_TRUE(selfMonitor->mPromMetricsMap.count("test-key"));
+    selfMonitor->InitMetricManager(testMetricKeys, MetricLabels{});
 
-    selfMonitor->CounterAdd("test-key", PROM_SUBSCRIBE_TOTAL, 200, 999);
+    selfMonitor->CounterAdd(PROM_SUBSCRIBE_TOTAL, 200, 999);
 
     // check result
-    auto metric = selfMonitor->mPromStatusMap["test-key"][200]->GetCounter(PROM_SUBSCRIBE_TOTAL);
+    auto metric = selfMonitor->mPromStatusMap[200]->GetCounter(PROM_SUBSCRIBE_TOTAL);
     APSARA_TEST_EQUAL("prom_subscribe_total", metric->GetName());
     APSARA_TEST_EQUAL(999ULL, metric->GetValue());
-    selfMonitor->CounterAdd("test-key", PROM_SUBSCRIBE_TOTAL, 200);
+    selfMonitor->CounterAdd(PROM_SUBSCRIBE_TOTAL, 200);
     APSARA_TEST_EQUAL(1000ULL, metric->GetValue());
 }
 
@@ -47,22 +34,20 @@ void PromSelfMonitorUnittest::TestIntGaugeSet() {
     std::unordered_map<std::string, MetricType> testMetricKeys = {
         {PROM_SUBSCRIBE_TARGETS, MetricType::METRIC_TYPE_INT_GAUGE},
     };
-    selfMonitor->InitMetricManager("test-key", testMetricKeys, MetricLabels{});
-    APSARA_TEST_TRUE(selfMonitor->mPromMetricsMap.count("test-key"));
+    selfMonitor->InitMetricManager(testMetricKeys, MetricLabels{});
 
     auto metricLabels = std::map<std::string, std::string>({{"test-label", "test-value"}});
-    selfMonitor->IntGaugeSet("test-key", PROM_SUBSCRIBE_TARGETS, 200, 999);
+    selfMonitor->IntGaugeSet(PROM_SUBSCRIBE_TARGETS, 200, 999);
 
     // check result
-    auto metric = selfMonitor->mPromStatusMap["test-key"][200]->GetIntGauge(PROM_SUBSCRIBE_TARGETS);
+    auto metric = selfMonitor->mPromStatusMap[200]->GetIntGauge(PROM_SUBSCRIBE_TARGETS);
     APSARA_TEST_EQUAL("prom_subscribe_targets", metric->GetName());
     APSARA_TEST_EQUAL(999ULL, metric->GetValue());
-    selfMonitor->IntGaugeSet("test-key", PROM_SUBSCRIBE_TARGETS, 200, 0);
+    selfMonitor->IntGaugeSet(PROM_SUBSCRIBE_TARGETS, 200, 0);
     APSARA_TEST_EQUAL(0ULL, metric->GetValue());
 }
 
 
-UNIT_TEST_CASE(PromSelfMonitorUnittest, TestInitMetricManager)
 UNIT_TEST_CASE(PromSelfMonitorUnittest, TestCounterAdd)
 UNIT_TEST_CASE(PromSelfMonitorUnittest, TestIntGaugeSet)
 
