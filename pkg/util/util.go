c80d66e4bc0505ed02e7c574ebaa50ef44311f28
QianyongXu <13038089398@163.com>
2024-09-19 01:08:58 -0700

    add String2Bytes func

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -408,3 +408,13 @@ func Contains[T comparable](s []T, e T) bool {
 	}
 	return false
 }
+
+func String2Bytes(s string) []byte {
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s)) //nolint
+	bh := reflect.SliceHeader{
+		Data: sh.Data,
+		Len:  sh.Len,
+		Cap:  sh.Len,
+	}
+	return *(*[]byte)(unsafe.Pointer(&bh)) //nolint
+}
