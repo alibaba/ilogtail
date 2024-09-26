157b8ef71eaff0b99a110a4d1b44a39b117644b9
裘文成 <1191890118@qq.com>
2024-09-26 06:44:14 +0000

    Crash临时文件：/var/lib/loongcollector/backtrace.dat

--- core/common/CrashBackTraceUtil.cpp
+++ core/common/CrashBackTraceUtil.cpp
@@ -13,8 +13,9 @@
 // limitations under the License.
 
 #include "CrashBackTraceUtil.h"
-#include <cstdlib>
+
 #include <cstdio>
+#include <cstdlib>
 #if defined(__ANDROID__)
 #elif defined(__linux__)
 #define UNW_LOCAL_ONLY
@@ -24,11 +25,12 @@
 #include <breakpad/client/windows/handler/exception_handler.h>
 #include <direct.h>
 #endif
-#include "logger/Logger.h"
-#include "RuntimeUtil.h"
 #include "Flags.h"
+#include "RuntimeUtil.h"
+#include "logger/Logger.h"
 
 DEFINE_FLAG_STRING(crash_stack_file_name, "crash stack back trace file name", "backtrace.dat");
+DECLARE_FLAG_STRING(loongcollector_data_dir);
 
 namespace logtail {
 
@@ -101,7 +103,7 @@ bool MinidumpCallbackFunc(const wchar_t* dump_path,
                           MDRawAssertionInfo* assertion,
                           bool succeeded) {
     printf("MinidumpCallbackFunc is called\n");
-    auto trgFilePath = GetProcessExecutionDir() + STRING_FLAG(crash_stack_file_name);
+    auto trgFilePath = STRING_FLAG(loongcollector_data_dir) + STRING_FLAG(crash_stack_file_name);
     if (0 == _access(trgFilePath.c_str(), 0)) {
         if (remove(trgFilePath.c_str()) != 0) {
             printf("Remove existing target file %s failed: %d", trgFilePath.c_str(), errno);
@@ -126,7 +128,8 @@ bool MinidumpCallbackFunc(const wchar_t* dump_path,
 void InitCrashBackTrace() {
 #if defined(__ANDROID__)
 #elif defined(__linux__)
-    g_crashBackTraceFilePtr = fopen((GetProcessExecutionDir() + STRING_FLAG(crash_stack_file_name)).c_str(), "w");
+    g_crashBackTraceFilePtr
+        = fopen((STRING_FLAG(loongcollector_data_dir) + STRING_FLAG(crash_stack_file_name)).c_str(), "w");
     if (g_crashBackTraceFilePtr == NULL) {
         APSARA_LOG_ERROR(sLogger, ("unable to open stack back trace file", strerror(errno)));
         return;
@@ -143,7 +146,7 @@ void InitCrashBackTrace() {
 }
 
 std::string GetCrashBackTrace() {
-    auto stackFilePath = GetProcessExecutionDir() + STRING_FLAG(crash_stack_file_name);
+    auto stackFilePath = STRING_FLAG(loongcollector_data_dir) + STRING_FLAG(crash_stack_file_name);
     FILE* pStackFile = fopen(stackFilePath.c_str(), "rb");
     if (pStackFile == NULL) {
         return "";
