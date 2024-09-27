9e965be09e415652ec710d2c1419211d65864370
Takuka0311 <1914426213@qq.com>
2024-09-27 18:02:28 +0800

    change name

--- core/monitor/metric_constants/ComponentMetrics.cpp
+++ core/monitor/metric_constants/ComponentMetrics.cpp
@@ -57,7 +57,7 @@ const string METRIC_COMPONENT_BATCHER_BUFFERED_SIZE_BYTES = "component_buffered_
  **********************************************************/
 const string METRIC_COMPONENT_QUEUE_SIZE = "component_queue_size";
 const string METRIC_COMPONENT_QUEUE_SIZE_BYTES = "component_queue_size_bytes";
-const string METRIC_COMPONENT_QUEUE_VALID_TO_PUSH_FLAG = "component_valid_to_push";
+const string METRIC_COMPONENT_QUEUE_VALID_TO_PUSH_FLAG = "component_valid_to_push_status";
 const string METRIC_COMPONENT_QUEUE_EXTRA_BUFFER_SIZE = "component_extra_buffer_size";
 const string METRIC_COMPONENT_QUEUE_EXTRA_BUFFER_SIZE_BYTES = "component_extra_buffer_size_bytes";
 const string METRIC_COMPONENT_QUEUE_DISCARDED_EVENTS_TOTAL = "component_discarded_events_total";
