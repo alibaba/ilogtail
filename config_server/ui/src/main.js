a42e9497a4fd9a917e1156cd5ffc68fa291fc00d
Wei Wu <73374361+ww67652@users.noreply.github.com>
2024-09-28 19:17:46 +0800

    Delete config_server/ui directory

--- config_server/ui/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import App from './App.vue'
-
-
-let app=createApp(App)
-app.use(ElementPlus)
-app.mount('#app')
