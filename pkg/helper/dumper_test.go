1155c8bf19d34f0d1e0f2e8b7b8848f9dd823dd3
quzard <1191890118@qq.com>
2024-10-09 12:29:12 +0000

    LoongcollectorConfDir

--- pkg/helper/dumper_test.go
+++ pkg/helper/dumper_test.go
@@ -31,9 +31,9 @@ import (
 )
 
 func TestServiceHTTP_doDumpFile(t *testing.T) {
-	_, err := os.Stat(path.Join(config.LoongcollectorGlobalConfig.LoongcollectorSysConfDir, "dump"))
+	_, err := os.Stat(path.Join(config.LoongcollectorGlobalConfig.LoongcollectorConfDir, "dump"))
 	if err == nil {
-		files, findErr := GetFileListByPrefix(path.Join(config.LoongcollectorGlobalConfig.LoongcollectorSysConfDir, "dump"), "custom", true, 0)
+		files, findErr := GetFileListByPrefix(path.Join(config.LoongcollectorGlobalConfig.LoongcollectorConfDir, "dump"), "custom", true, 0)
 		require.NoError(t, findErr)
 		for _, file := range files {
 			_ = os.Remove(file)
