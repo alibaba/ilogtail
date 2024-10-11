b120b9b28ea923a56eda6c175a5d00cb69555a38
quzard <1191890118@qq.com>
2024-10-11 08:16:14 +0000

    fix

--- pkg/helper/dumper_test.go
+++ pkg/helper/dumper_test.go
@@ -31,9 +31,9 @@ import (
 )
 
 func TestServiceHTTP_doDumpFile(t *testing.T) {
-	_, err := os.Stat(path.Join(util.GetCurrentBinaryPath(), "exec_dir", "dump"))
+	_, err := os.Stat(path.Join(util.GetCurrentBinaryPath(), "dump"))
 	if err == nil {
-		files, findErr := GetFileListByPrefix(path.Join(util.GetCurrentBinaryPath(), "exec_dir", "dump"), "custom", true, 0)
+		files, findErr := GetFileListByPrefix(path.Join(util.GetCurrentBinaryPath(), "dump"), "custom", true, 0)
 		require.NoError(t, findErr)
 		for _, file := range files {
 			_ = os.Remove(file)
