d14496621cdc4b8bafe13491f41763305e75e348
henryzhx8 <zhhxjack8@aliyun.com>
2024-11-13 07:18:01 +0000

    polish

--- core/config/watcher/ConfigWatcher.h
+++ core/config/watcher/ConfigWatcher.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 iLogtail Authors
+ * Copyright 2024 iLogtail Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
