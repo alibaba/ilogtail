12bfe971e51583b1b32efdeadaaf318c99348c71
github-action-benchmark <github@users.noreply.github.com>
2024-11-29 07:35:55 +0000

    add benchmark (customSmallerIsBetter) benchmark result for 3fb953173335e0472c8d750c6ea1888d1ace7b14

---
+++ dev/bench/data.js
@@ -0,0 +1,48 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1732865755194,
+  "repoUrl": "https://github.com/alibaba/loongcollector",
+  "entries": {
+    "benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "alibaba",
+            "username": "alibaba"
+          },
+          "committer": {
+            "name": "alibaba",
+            "username": "alibaba"
+          },
+          "id": "3fb953173335e0472c8d750c6ea1888d1ace7b14",
+          "message": "Update README.md rename to LoongCollector",
+          "timestamp": "2024-11-29T06:02:49Z",
+          "url": "https://github.com/alibaba/loongcollector/pull/1931/commits/3fb953173335e0472c8d750c6ea1888d1ace7b14"
+        },
+        "date": 1732865754693,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CPU_Usage_Max - performance_file_to_blackhole_ilogtail",
+            "value": 44.78444703919242,
+            "unit": "%"
+          },
+          {
+            "name": "CPU_Usage_Avg - performance_file_to_blackhole_ilogtail",
+            "value": 38.37790610707261,
+            "unit": "%"
+          },
+          {
+            "name": "Memory_Usage_Max - performance_file_to_blackhole_ilogtail",
+            "value": 47.31640625,
+            "unit": "MB"
+          },
+          {
+            "name": "Memory_Usage_Avg - performance_file_to_blackhole_ilogtail",
+            "value": 42.7149325284091,
+            "unit": "MB"
+          }
+        ]
+      }
+    ]
+  }
+}
